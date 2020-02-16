8e100a14068c58a476e120df5a17c9b3d1b7146a
Anders Magnusson <anders14magnusson@gmail.com>
2020-02-16 23:35:59 +0100

    Add basic ECS player-controlled movement
    Player movement implementation for testing purposes.

---
+++ Assets/Scripts/ECS/Player/PlayerInputSystem.cs
@@ -0,0 +1,35 @@
+﻿using Ecosystem.ECS.Movement;
+using Unity.Entities;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace Ecosystem.ECS.Player
+{
+    /// <summary>
+    /// Movement input controls for entities with a PlayerTag component.
+    /// </summary>
+    public class PlayerInputSystem : ComponentSystem
+    {
+        protected override void OnUpdate()
+        {
+            float x = Input.GetAxisRaw("Horizontal");
+            float z = Input.GetAxisRaw("Vertical");
+            bool sprint = Input.GetKey(KeyCode.LeftShift);
+            float3 direction = new float3(x, 0f, z);
+            if (math.length(direction) != 0)
+            {
+                direction = math.normalize(direction);
+            }
+
+            Entities.ForEach((
+                ref MovementInput movementInput,
+                ref PlayerTag playerTag) =>
+            {
+
+                movementInput.Direction = direction;
+                movementInput.Sprint = sprint;
+
+            });
+        }
+    }
+}
