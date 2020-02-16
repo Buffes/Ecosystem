fb56e21e07bf5a87dfafc06bde0cde95d87d8a80
Anders Magnusson <anders14magnusson@gmail.com>
2020-02-16 23:34:23 +0100

    Add basic ECS movement

---
+++ Assets/Scripts/ECS/Movement/MoveCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Ecosystem.ECS.Movement
+{
+    /// <summary>
+    /// Target position to find a path and move to.
+    /// </summary>
+    [Serializable]
+    public struct MoveCommand : IComponentData
+    {
+        public float3 Target;
+    }
+}
---
+++ Assets/Scripts/ECS/Movement/Pathfinding/PathfindingSystem.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Ecosystem.ECS.Movement.Pathfinding
+{
+    public class PathfindingSystem : JobComponentSystem
+    {
+        protected override JobHandle OnUpdate(JobHandle inputDependencies)
+        {
+            return Entities.ForEach((
+                ref MovementInput movementInput) =>
+            {
+
+                // Logic goes here
+
+            }).Schedule(inputDependencies);
+        }
+    }
+}
