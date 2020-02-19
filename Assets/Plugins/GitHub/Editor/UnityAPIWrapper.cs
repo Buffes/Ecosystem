b2bc86c952392dc48930a30ab66c63d8efc08791
Edvin Leidö <14072276+Buffes@users.noreply.github.com>
2020-02-19 22:57:41 +0100

    Began work on pathfinding using value types

--- Assets/Plugins/GitHub/Editor/UnityAPIWrapper.cs
+++ Assets/Plugins/GitHub/Editor/UnityAPIWrapper.cs
@@ -11,9 +11,9 @@ namespace GitHub.Unity
         static UnityAPIWrapper()
         {
 #if UNITY_2018_2_OR_NEWER
-            Editor.finishedDefaultHeaderGUI += editor => {
-                UnityShim.Raise_Editor_finishedDefaultHeaderGUI(editor);
-            };
+            // Editor.finishedDefaultHeaderGUI += editor => {
+            //     UnityShim.Raise_Editor_finishedDefaultHeaderGUI(editor);
+            // };
 #endif
         }
     }
--- Assets/Scripts/ECS/Movement/Pathfinding/PathfindingSystem.cs
+++ Assets/Scripts/ECS/Movement/Pathfinding/PathfindingSystem.cs
@@ -12,6 +12,9 @@ namespace Ecosystem.ECS.Movement.Pathfinding
     /// </summary>
     public class PathfindingSystem : JobComponentSystem
     {
+        private const int MOVE_STRAIGHT_COST = 10;
+        private const int MOVE_DIAGONAL_COST = 14; // Approximate sqrt(2) as an int
+
         EndSimulationEntityCommandBufferSystem m_EndSimulationEcbSystem;
 
         protected override void OnCreate()
@@ -51,6 +54,77 @@ namespace Ecosystem.ECS.Movement.Pathfinding
             return jobHandle;
         }
 
+        ///<summary>
+        /// Path finding using a basic, unoptimized version of the A* algorithm. Uses solely value types for Burst compatibility.
+        ///<summary>
+        private void FindPath(int2 startPosition, int2 targetPosition)
+        {
+            // Temporary representation of the grid, mapping coordinates to an int for walkability. 
+            // 0 = not walkable, 1 = walkable. We assume equal weights for all positions in the grid for now.
+            // TODO: Change this into reading from the grid when that is implemented. 
+            int2 gridSize = new int2(100, 100);
+            NativeHashMap<int2, int> pathFindingMap = new NativeHashMap<int2, int>(gridSize.x * gridSize.y, Allocator.Temp);
+            
+            NativeList<PathNode> pathNodes = new NativeList<PathNode>(Allocator.Temp); // Add pathNodes to this lazily as needed
+            int index = 0;
+
+            NativeList<int> openList = new NativeList<int>(Allocator.Temp);
+            NativeHashMap<int, int> closedList = new NativeHashMap<int, int>(Allocator.Temp);
+
+            pathNodes.Add(MakePathNode(startPosition, ref index, Heuristic(startPosition, targetPosition), 0));
+
+            openList.Add(pathNodes[0].index);
+
+            while (openList.Length > 0)
+            {
+                int currentNode = GetLowestFCostNodeIndex(openList, pathNodes);
+
+                if ()
+            }
+
+            openList.Dispose();
+            closedList.Dispose();
+            pathNodes.Dispose();
+            pathFindingMap.Dispose(); // Dispose the temporary map
+        }
+
+        private int Heuristic(int2 a, int2 b) {
+            int dx = math.abs(a.x - b.x);
+            int dy = math.abs(a.y - b.y);
+            
+            // Disregards walls to give an estimated cost.
+            return MOVE_DIAGONAL_COST * math.min(dx, dy) + MOVE_STRAIGHT_COST * math.abs(dx - dy);
+;
+        }
+
+        private PathNode MakePathNode(int2 position, ref int index, int hCost, int gCost)
+        {
+            PathNode node = new PathNode();
+            node.x = position.x;
+            node.y = position.y;
+            node.index = index;
+            index++;
+            node.gCost = gCost;
+            node.hCost = hCost;
+            node.CalculateFCost();
+            node.cameFromIndex = -1;
+
+            return node;
+        }
+
+        // adasd
+        private int GetLowestFCostNodeIndex(NativeList<int> openList, NativeList<PathNode> pathNodes) 
+        {
+            PathNode lowestCostPathNode = pathNodes[openList[0]];
+            for (int i = 1; i < openList.Length; i++) {
+                PathNode testPathNode = pathNodes[openList[i]];
+                if (testPathNode.fCost < lowestCostPathNode.fCost) {
+                    lowestCostPathNode = testPathNode;
+                }
+            }
+            return lowestCostPathNode.index;
+        }
+
         private static int2 GetGridCoords(float3 worldPosition)
         {
             int x = (int)worldPosition.x - (worldPosition.x < 0 ? 1 : 0);
@@ -63,6 +137,22 @@ namespace Ecosystem.ECS.Movement.Pathfinding
             float x = gridCoords.x + 0.5f;
             float z = gridCoords.y + 0.5f;
             return new float3(x, 0f, z);
+        }
+
+        private struct PathNode 
+        {
+            public int x;
+            public int y;
+            public int index;
+
+            public int gCost;
+            public int hCost;
+            public int fCost;
+            public int cameFromIndex;
+
+            public void CalculateFCost() {
+                fCost = gCost + hCost;
+            }
         }
     }
 }
