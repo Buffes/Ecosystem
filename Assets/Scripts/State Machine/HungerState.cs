18dc07e1e889b65eed0341f063afe747ab78afd1
Kasper Hall <kasperhall@hotmail.com>
2020-02-20 13:48:37 +0100

    First try on a hunger state

--- Assets/Scripts/State Machine/HungerState.cs
+++ Assets/Scripts/State Machine/HungerState.cs
@@ -13,6 +13,25 @@ namespace Ecosystem.StateMachines {
         }
 
         public void Execute() {
+            bool foundFood = false;
+
+            while (!foundFood)
+            {
+                //This will be changed to work with the field of view method
+                var hitObjects = Physics.OverlapSphere(this.owner.transform.position, 30f);
+                for (int i = 0; i < hitObjects.Length; i++)
+                {
+                    if (hitObjects[i].CompareTag(owner.FoodSource))
+                    {
+                        //move to hitObjects[i].transform.postion and eat it
+                        //goes to the first obejct in the list and does not take distance into consideration
+                        foundFood = true;
+                    }
+                    break;
+                }
+
+                //walk at random or by memory to find food
+            }
 
         }
 
