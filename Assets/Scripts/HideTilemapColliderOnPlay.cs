3109f808c8bdb542c4a2745eeeb5bc05be8c7c7f
SemlanChalmers <31894663+SemlanChalmers@users.noreply.github.com>
2020-02-21 08:55:37 +0100

    Tilemap updates

---
+++ Assets/Scripts/HideTilemapColliderOnPlay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+
+public class HideTilemapColliderOnPlay : MonoBehaviour
+{
+    private TilemapRenderer tilemapRenderer;
+
+    void Start() {
+        tilemapRenderer = GetComponent<TilemapRenderer>();
+        tilemapRenderer.enabled = false;
+    }
+}
