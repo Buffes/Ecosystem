c2a521b21419c63275af039b59e38095a06cfebf
Anders Magnusson <anders14magnusson@gmail.com>
2020-02-21 21:27:32 +0100

    Add hybrid ECS movement interface

---
+++ Assets/Scripts/ECS/Hybrid/Movement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Ecosystem.ECS.Hybrid
+{
+    public class Movement : MonoBehaviour
+    {
+        /// <summary>
+        /// Sends a move command to move to the specified target position.
+        /// Avoid calling this too often because for every frame that it is called in,
+        /// a new path will be calculated.
+        /// </summary>
+        /// <param name="target">the target position to move to</param>
+        public void Move(Vector3 target)
+        {
+        }
+    }
+}
