d19fc138a294f7c3cc730254d9e8c48e67029f2c
Anders Magnusson <anders14magnusson@gmail.com>
2020-02-22 12:36:38 +0100

    Add AnimalType scriptable object

---
+++ Assets/ScriptableObjects/AnimalType.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ecosystem
+{
+    [CreateAssetMenu(menuName = "AnimalType")]
+    public class AnimalType : ScriptableObject
+    {
+        [Tooltip("Animals that are prey to this animal type")]
+        public List<AnimalType> Prey = new List<AnimalType>();
+
+        [Tooltip("Food that is edible for this animal type")]
+        public List<FoodType> Food = new List<FoodType>();
+    }
+}
