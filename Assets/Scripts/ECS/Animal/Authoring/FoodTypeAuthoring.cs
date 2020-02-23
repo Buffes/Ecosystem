4bbabbf8bf8927e7da1dbec1a2024770ded8b2d8
Anders Magnusson <anders14magnusson@gmail.com>
2020-02-23 16:02:11 +0100

    Add animal type ECS components

---
+++ Assets/Scripts/ECS/Animal/Authoring/FoodTypeAuthoring.cs
@@ -0,0 +1,17 @@
+﻿using Ecosystem.Gameplay;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Ecosystem.ECS.Animal
+{
+    public class FoodTypeAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+    {
+        [SerializeField]
+        private FoodType foodType = default;
+
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            dstManager.AddComponentData(entity, new FoodTypeData { FoodTypeId = foodType.GetInstanceID() });
+        }
+    }
+}
