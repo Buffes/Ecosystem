cf59f76cadd693deddb00e57cda0fc7b1afc49ee
Edvin Leidö <14072276+Buffes@users.noreply.github.com>
2020-02-23 17:00:59 +0100

    Added object oriented implementation of pathfinding for presentation

---
+++ Assets/Scripts/Demonstration/PathNode.cs
@@ -0,0 +1,39 @@
+﻿/* Largely copied from Code Monkey */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode {
+
+    private Grid<PathNode> grid;
+    public int x;
+    public int y;
+
+    public int gCost;
+    public int hCost;
+    public int fCost;
+
+    public bool isWalkable;
+    public PathNode cameFromNode;
+
+    public PathNode(Grid<PathNode> grid, int x, int y) {
+        this.grid = grid;
+        this.x = x;
+        this.y = y;
+        isWalkable = true;
+    }
+
+    public void CalculateFCost() {
+        fCost = gCost + hCost;
+    }
+
+    public void SetIsWalkable(bool isWalkable) {
+        this.isWalkable = isWalkable;
+    }
+
+    public override string ToString() {
+        return x + "," + y;
+    }
+
+}
