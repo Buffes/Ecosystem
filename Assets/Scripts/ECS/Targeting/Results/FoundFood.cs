f566df810eb22ac6da65407ca5048ac61218dffd
Anders Magnusson <anders14magnusson@gmail.com>
2020-02-24 20:36:31 +0100

    Add found target result components

---
+++ Assets/Scripts/ECS/Targeting/Results/FoundFood.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Ecosystem.ECS.Targeting.Results
+{
+    [Serializable]
+    public struct FoundFood : IComponentData
+    {
+        public float3 Position;
+    }
+}
