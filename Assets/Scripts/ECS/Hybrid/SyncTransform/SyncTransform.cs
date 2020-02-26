30de68b86399ca555fe97f525fa94c15d3e71487
Anders Magnusson <anders14magnusson@gmail.com>
2020-02-26 09:20:54 +0100

    Add system for syncing gameobject position with entity

---
+++ Assets/Scripts/ECS/Hybrid/SyncTransform/SyncTransform.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Ecosystem.ECS.Hybrid.SyncTransform
+{
+    /// <summary>
+    /// A GameObject's Transform to sync the position of with this entity.
+    /// </summary>
+    [Serializable]
+    [GenerateAuthoringComponent]
+    public class SyncTransform : IComponentData
+    {
+        public Transform transform;
+    }
+}
---
+++ Assets/Scripts/ECS/Hybrid/SyncTransform/SyncTransformSystem.cs
@@ -0,0 +1,27 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+
+namespace Ecosystem.ECS.Hybrid.SyncTransform
+{
+    /// <summary>
+    /// Syncs the position of a game object's transform with an entity's translation.
+    /// </summary>
+    public class SyncTransformSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            Entities
+                .WithoutBurst()
+                .ForEach((
+                    SyncTransform syncTransform,
+                    in Translation position,
+                    in Rotation rotation) =>
+            {
+
+                syncTransform.transform.position = position.Value;
+                syncTransform.transform.rotation = rotation.Value;
+
+            }).Run();
+        }
+    }
+}
