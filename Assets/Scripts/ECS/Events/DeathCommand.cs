3920f7fda581ee245be50152a748281c6fab8f8d
Slarvsylt <42959581+Slarvsylt@users.noreply.github.com>
2020-02-27 17:25:35 +0100

    Started on a death-system

---
+++ Assets/Scripts/ECS/Events/DeathCommand.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Entities;
+
+namespace Assets.Scripts.ECS.Events
+{
+    /// <summary>
+    /// Commands the entity to die
+    /// </summary>
+    public struct DeathCommand : IComponentData
+    {
+
+    }
+}
---
+++ Assets/Scripts/ECS/Events/DeathEventSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Collections;
+
+namespace Assets.Scripts.ECS.Events
+{
+    /// <summary>
+    /// Kills desired entities
+    /// </summary>
+    public class DeathEventSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            Entities.ForEach((Entity entity, in DeathCommand death) =>
+            {
+                
+            }).ScheduleParallel();
+        }
+    }
+}
