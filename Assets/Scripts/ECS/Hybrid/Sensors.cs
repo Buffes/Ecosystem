c58cbc76829f145128926fc4ac3fd0b4725bd85f
Anders Magnusson <anders14magnusson@gmail.com>
2020-03-01 00:35:11 +0100

    Refactor use expression body for methods

--- Assets/Scripts/ECS/Hybrid/Sensors.cs
+++ Assets/Scripts/ECS/Hybrid/Sensors.cs
@@ -23,50 +23,20 @@ namespace Ecosystem.ECS.Hybrid
         /// <summary>
         /// Start/stop actively looking for water.
         /// </summary>
-        public void LookForWater(bool enabled)
-        {
-            entityManager.AddComponentData(entity, new LookingForWater());
-        }
-
-        public void LookForFood(bool enabled)
-        {
-            entityManager.AddComponentData(entity, new LookingForFood());
-        }
-
-        public void LookForPrey(bool enabled)
-        {
-            entityManager.AddComponentData(entity, new LookingForPrey());
-        }
-
-        public void LookForPredator(bool enabled)
-        {
-            AddRemoveComponentData(enabled, new LookingForPredator());
-        }
+        public void LookForWater(bool enabled) => entityManager.AddComponentData(entity, new LookingForWater());
+        public void LookForFood(bool enabled) => entityManager.AddComponentData(entity, new LookingForFood());
+        public void LookForPrey(bool enabled) => entityManager.AddComponentData(entity, new LookingForPrey());
+        public void LookForPredator(bool enabled) => AddRemoveComponentData(enabled, new LookingForPredator());
 
 
         /// <summary>
         /// Returns if water has been found. Make sure to have enabled
         /// <see cref="LookForWater(bool)"/> first.
         /// </summary>
-        public bool FoundWater()
-        {
-            return entityManager.HasComponent<FoundWater>(entity);
-        }
-
-        public bool FoundFood()
-        {
-            return entityManager.HasComponent<FoundFood>(entity);
-        }
-
-        public bool FoundPrey()
-        {
-            return entityManager.HasComponent<FoundPrey>(entity);
-        }
-
-        public bool FoundPredator()
-        {
-            return entityManager.HasComponent<FoundPredator>(entity);
-        }
+        public bool FoundWater() => entityManager.HasComponent<FoundWater>(entity);
+        public bool FoundFood() => entityManager.HasComponent<FoundFood>(entity);
+        public bool FoundPrey() => entityManager.HasComponent<FoundPrey>(entity);
+        public bool FoundPredator() => entityManager.HasComponent<FoundPredator>(entity);
 
 
         /// <summary>
@@ -74,53 +44,23 @@ namespace Ecosystem.ECS.Hybrid
         /// Make sure that water has been found first by checking <see cref="FoundWater()"/>.
         /// </summary>
         [Obsolete("Use GetFoundWaterInfo().Position instead")]
-        public Vector3 GetWaterLocation()
-        {
-            return entityManager.GetComponentData<FoundWater>(entity).Position;
-        }
-
+        public Vector3 GetWaterLocation() => entityManager.GetComponentData<FoundWater>(entity).Position;
         [Obsolete("Use GetFoundFoodInfo().Position instead")]
-        public Vector3 GetFoodLocation()
-        {
-            return entityManager.GetComponentData<FoundFood>(entity).Position;
-        }
-
+        public Vector3 GetFoodLocation() => entityManager.GetComponentData<FoundFood>(entity).Position;
         [Obsolete("Use GetFoundPreyInfo().Position instead")]
-        public Vector3 GetPreyLocation()
-        {
-            return entityManager.GetComponentData<FoundPrey>(entity).Position;
-        }
-
+        public Vector3 GetPreyLocation() => entityManager.GetComponentData<FoundPrey>(entity).Position;
         [Obsolete("Use GetFoundPredatorInfo().Position instead")]
-        public Vector3 GetPredatorLocation()
-        {
-            return entityManager.GetComponentData<FoundPredator>(entity).Position;
-        }
+        public Vector3 GetPredatorLocation() => entityManager.GetComponentData<FoundPredator>(entity).Position;
 
 
         /// <summary>
         /// Returns info about the water that has been found.
         /// Make sure that water has been found first by checking <see cref="FoundWater()"/>.
         /// </summary>
-        public FoundWater GetFoundWaterInfo()
-        {
-            return entityManager.GetComponentData<FoundWater>(entity);
-        }
-
-        public FoundFood GetFoundFoodInfo()
-        {
-            return entityManager.GetComponentData<FoundFood>(entity);
-        }
-
-        public FoundPrey GetFoundPreyInfo()
-        {
-            return entityManager.GetComponentData<FoundPrey>(entity);
-        }
-
-        public FoundPredator GetFoundPredatorInfo()
-        {
-            return entityManager.GetComponentData<FoundPredator>(entity);
-        }
+        public FoundWater GetFoundWaterInfo() => entityManager.GetComponentData<FoundWater>(entity);
+        public FoundFood GetFoundFoodInfo() => entityManager.GetComponentData<FoundFood>(entity);
+        public FoundPrey GetFoundPreyInfo() => entityManager.GetComponentData<FoundPrey>(entity);
+        public FoundPredator GetFoundPredatorInfo() => entityManager.GetComponentData<FoundPredator>(entity);
 
 
         private void AddRemoveComponentData<T>(bool add, T component) where T : struct, IComponentData
