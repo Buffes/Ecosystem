6eb18473785ff1c1f4c32120db9232028f9d36a6
Anders Magnusson <anders14magnusson@gmail.com>
2020-03-02 12:40:29 +0100

    Add meat prefab to animal type

--- Assets/Scripts/ECS/Animal/Authoring/AnimalTypeAuthoring.cs
+++ Assets/Scripts/ECS/Animal/Authoring/AnimalTypeAuthoring.cs
@@ -1,17 +1,26 @@
 ﻿using Ecosystem.Gameplay;
+using System.Collections.Generic;
 using Unity.Entities;
 using UnityEngine;
 
 namespace Ecosystem.ECS.Animal
 {
-    public class AnimalTypeAuthoring : MonoBehaviour, IConvertGameObjectToEntity
+    public class AnimalTypeAuthoring : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
     {
         [SerializeField]
         private AnimalType animalType = default;
 
         public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
         {
-            dstManager.AddComponentData(entity, new AnimalTypeData { AnimalTypeId = animalType.GetInstanceID() });
+            dstManager.AddComponentData(entity, new AnimalTypeData
+            {
+                AnimalTypeId = animalType.GetInstanceID()
+            });
+
+            dstManager.AddComponentData(entity, new DropOnDeath
+            {
+                Prefab = conversionSystem.GetPrimaryEntity(animalType.Meat)
+            });
 
             DynamicBuffer<PreyTypesElement> preyBuffer = dstManager.AddBuffer<PreyTypesElement>(entity);
             foreach (AnimalType animalType in animalType.Prey)
@@ -28,6 +37,11 @@ namespace Ecosystem.ECS.Animal
 
                 foodBuffer.Add(new FoodTypesElement { FoodTypeId = foodType.GetInstanceID() });
             }
+        }
+
+        public void DeclareReferencedPrefabs(List<GameObject> referencedPrefabs)
+        {
+            referencedPrefabs.Add(animalType.Meat);
         }
     }
 }
