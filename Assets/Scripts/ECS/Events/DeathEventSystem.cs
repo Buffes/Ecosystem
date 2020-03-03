b940e7de7d4b3fd80238e653b2d5661a1f44b2bc
Jesper Berglind <jesper.berglind@gmail.com>
2020-03-03 10:20:38 +0100

    Removed obsolete code

--- Assets/Scripts/ECS/Events/DeathEventSystem.cs
+++ Assets/Scripts/ECS/Events/DeathEventSystem.cs
@@ -14,30 +14,14 @@ namespace Ecosystem.ECS.Events
     /// </summary>
     public class DeathEventSystem : ComponentSystem
     {
-        EndSimulationEntityCommandBufferSystem m_EndSimulationEcbSystem;
-        private EntityManager entityManager;
-
-        protected override void OnCreate()
-        {
-            base.OnCreate();
-            m_EndSimulationEcbSystem = World
-                .GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-        }
-        
         protected override void OnUpdate()
         {
-            var commandBuffer = m_EndSimulationEcbSystem.CreateCommandBuffer().ToConcurrent();
 
             Entities.WithAll<DeathCommand>().ForEach((Entity entity, /*int entityInQueryIndex,*/
                 ref DeathCommand deathCmd) =>
             {
                 PostUpdateCommands.DestroyEntity(deathCmd.target);
                 PostUpdateCommands.DestroyEntity(entity);
-
-                //CommandBuffer or EntityManager??????
-
-               /* commandBuffer.DestroyEntity(entityInQueryIndex, entity);
-                commandBuffer.DestroyEntity(entityInQueryIndex, entity);*/
 
             });
         }
