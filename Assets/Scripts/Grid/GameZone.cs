611a471bf6b894f3338fd49a8d7a332f8e6bb463
Alexander <alesel@student.chalmers.se>
2020-03-04 14:19:13 +0100

    Different kinds of scenary added

--- Assets/Scripts/Grid/GameZone.cs
+++ Assets/Scripts/Grid/GameZone.cs
@@ -19,24 +19,94 @@ namespace Ecosystem.Grid
         private int diffWaterLand = 0;
 
         //The rate of chance for spawning an object if there is a similar object as neighbour
-        private float waterNeighbourRate = 0.7f;
-        private float greenScenaryNeigbourRate = 0.3f;
+        private float waterNeighbourRate = 0.65f;
+        private float greenScenaryNeigbourRate = 0.15f;
 
         //The rate of objects spawning
         private float waterSpawnRate = 0.005f;
-        private float greenScenarySpawnRate = 0.1f;
+        private float greenScenarySpawnRate = 0.05f;
         private float desertScenarySpawnRate = 0.1f;
-
-
-
 
         //Matrix of walkable tiles
         private bool [,] walkableTiles;
 
-        public GameObject bush;
-        public GameObject tree;
-        public GameObject rock;
-        public GameObject cactus;
+        public GameObject bush1;
+        public GameObject bush2;
+        public GameObject bush3;
+        public GameObject bush4;
+        public GameObject bush5;
+        public GameObject bush6;
+        public GameObject bush7;
+        public GameObject bush8;
+        public GameObject bush9;
+        public GameObject bush10;
+        public GameObject bush11;
+        public GameObject bush12;
+        public GameObject tree1;
+        public GameObject tree2;
+        public GameObject tree3;
+        public GameObject tree4;
+        public GameObject tree5;
+        public GameObject tree6;
+        public GameObject tree7;
+        public GameObject tree8;
+        public GameObject tree9;
+        public GameObject tree10;
+        public GameObject tree11;
+        public GameObject tree12;
+        public GameObject tree13;
+        public GameObject tree14;
+        public GameObject tree15;
+        public GameObject tree16;
+        public GameObject tree17;
+        public GameObject tree18;
+        public GameObject tree19;
+        public GameObject tree20;
+        public GameObject tree21;
+        public GameObject tree22;
+        public GameObject tree23;
+        public GameObject tree24;
+        public GameObject tree25;
+        public GameObject tree26;
+        public GameObject tree27;
+        public GameObject tree28;
+        public GameObject tree29;
+        public GameObject tree30;
+        public GameObject tree31;
+        public GameObject tree32;
+        public GameObject tree33;
+        public GameObject tree34;
+        public GameObject tree35;
+        public GameObject tree36;
+        public GameObject tree37;
+        public GameObject tree38;
+        public GameObject tree39;
+        public GameObject tree40;
+        public GameObject tree41;
+        public GameObject tree42;
+        public GameObject tree43;
+        public GameObject tree44;
+        public GameObject tree45;
+        public GameObject tree46;
+        public GameObject tree47;
+        public GameObject tree48;
+        public GameObject tree49;
+        public GameObject tree50;
+        public GameObject rock1;
+        public GameObject rock2;
+        public GameObject rock3;
+        public GameObject rock4;
+        public GameObject rock5;
+        public GameObject rock6;
+        public GameObject rock7;
+        public GameObject rock8;
+        public GameObject rock9;
+        public GameObject rock10;
+        public GameObject rock11;
+        public GameObject rock12;
+        public GameObject cactus1;
+        public GameObject cactus4;
+        public GameObject cactus5;
 
         //Matrix with all GameObjects in the Grid
         private Scenary [,] gameObjectsInGrid;
@@ -60,10 +130,58 @@ namespace Ecosystem.Grid
             walkableTiles = new bool [tiles.GetLength(0), tiles.GetLength(1)];
             gameObjectsInGrid = new Scenary [tiles.GetLength(0), tiles.GetLength(1)];
 
-            bush = Resources.Load("bush1") as GameObject;
-            tree = Resources.Load("tree1") as GameObject;
-            rock = Resources.Load("rock1") as GameObject;
-            cactus = Resources.Load("cactus1") as GameObject;
+            bush1 = Resources.Load("bush1") as GameObject;
+            bush2 = Resources.Load("bush2") as GameObject;
+            bush3 = Resources.Load("bush3") as GameObject;
+            bush4 = Resources.Load("bush4") as GameObject;
+            bush5 = Resources.Load("bush5") as GameObject;
+            bush6 = Resources.Load("bush6") as GameObject;
+            bush7 = Resources.Load("bush7") as GameObject;
+            bush8 = Resources.Load("bush8") as GameObject;
+            bush9 = Resources.Load("bush9") as GameObject;
+            tree1 = Resources.Load("tree1") as GameObject;
+            tree2 = Resources.Load("tree2") as GameObject;
+            tree3 = Resources.Load("tree3") as GameObject;
+            tree4 = Resources.Load("tree4") as GameObject;
+            tree5 = Resources.Load("tree5") as GameObject;
+            tree6 = Resources.Load("tree6") as GameObject;
+            tree7 = Resources.Load("tree7") as GameObject;
+            tree8 = Resources.Load("tree8") as GameObject;
+            tree9 = Resources.Load("tree9") as GameObject;
+            tree10 = Resources.Load("tree10") as GameObject;
+            tree11 = Resources.Load("tree11") as GameObject;
+            tree12 = Resources.Load("tree12") as GameObject;
+            tree13 = Resources.Load("tree13") as GameObject;
+            tree14 = Resources.Load("tree14") as GameObject;
+            tree15 = Resources.Load("tree15") as GameObject;
+            tree16 = Resources.Load("tree16") as GameObject;
+            tree17 = Resources.Load("tree17") as GameObject;
+            tree18 = Resources.Load("tree18") as GameObject;
+            tree19 = Resources.Load("tree19") as GameObject;
+            tree20 = Resources.Load("tree20") as GameObject;
+            tree21 = Resources.Load("tree21") as GameObject;
+            tree22 = Resources.Load("tree22") as GameObject;
+            tree23 = Resources.Load("tree23") as GameObject;
+            tree24 = Resources.Load("tree24") as GameObject;
+            tree25 = Resources.Load("tree25") as GameObject;
+            tree26 = Resources.Load("tree26") as GameObject;
+            tree27 = Resources.Load("tree27") as GameObject;
+            tree28 = Resources.Load("tree28") as GameObject;
+            tree29 = Resources.Load("tree29") as GameObject;
+            tree30 = Resources.Load("tree30") as GameObject;
+            rock1 = Resources.Load("rock1") as GameObject;
+            rock2 = Resources.Load("rock2") as GameObject;
+            rock3 = Resources.Load("rock3") as GameObject;
+            rock4 = Resources.Load("rock4") as GameObject;
+            rock5 = Resources.Load("rock5") as GameObject;
+            rock6 = Resources.Load("rock6") as GameObject;
+            rock7 = Resources.Load("rock7") as GameObject;
+            rock8 = Resources.Load("rock8") as GameObject;
+            rock9 = Resources.Load("rock9") as GameObject;
+            rock10 = Resources.Load("rock10") as GameObject;
+            rock11 = Resources.Load("rock11") as GameObject;
+            rock12 = Resources.Load("rock12") as GameObject;
+            cactus1 = Resources.Load("cactus1") as GameObject;
         }
 
         private void RandomizeStartGrid()
@@ -910,6 +1028,7 @@ namespace Ecosystem.Grid
 
         private void SetupGameObjects()
         {
+            //Create method for this;
             for (int row = 0; row < tiles.GetLength(0); row++)
             {
                 for (int col = 0; col < tiles.GetLength(1); col++)
@@ -928,17 +1047,17 @@ namespace Ecosystem.Grid
                     if (tiles[row, col] == 34)
                     {
                         //Scenary in the desert does not spawn more often if there are other things closeby
-                        gameObjectsInGrid[row, col] = RandomizeDesert(desertScenarySpawnRate, row, col);
+                        gameObjectsInGrid[row, col] = RandomizeDesertScenary(desertScenarySpawnRate, row, col);
                     }
                     else if (tiles[row, col] == 51)
                     {
                         if (ScenaryAsNeighbour(row, col) != Scenary.Empty)
                         {
-                            gameObjectsInGrid[row, col] = RandomizeGreen(greenScenaryNeigbourRate, row, col);
+                            gameObjectsInGrid[row, col] = RandomizeGreenScenary(greenScenaryNeigbourRate, row, col);
                         }
                         else 
                         {
-                            gameObjectsInGrid[row, col] = RandomizeGreen(greenScenarySpawnRate, row, col);
+                            gameObjectsInGrid[row, col] = RandomizeGreenScenary(greenScenarySpawnRate, row, col);
                         }
                     }
                 }
@@ -1008,7 +1127,7 @@ namespace Ecosystem.Grid
             return gameObjectsInGrid[row, col];
         }
 
-        private Scenary RandomizeGreen(float value, int row, int col)
+        private Scenary RandomizeGreenScenary(float value, int row, int col)
         {
             float newBush = value / 3;
             float newTree = (value / 3) * 2;
@@ -1017,19 +1136,22 @@ namespace Ecosystem.Grid
             
             if (rand <= newBush)
             {
-                Instantiate (bush, new Vector3(row, 0, col), Quaternion.identity);
+                GameObject ob = GetRandomBush();
+                Instantiate (ob, new Vector3(row, 0, col), Quaternion.identity);
                 walkableTiles[row, col] = false;
                 return Scenary.Bush;
             }
             else if (rand <= newTree)
             {
-                Instantiate (tree, new Vector3(row, 0, col), Quaternion.identity);
+                GameObject ob = GetRandomTree();
+                Instantiate (ob, new Vector3(row, 0, col), Quaternion.identity);
                 walkableTiles[row, col] = false;
                 return Scenary.Tree;
             }
             else if (rand <= newRock) 
             {
-                Instantiate (rock, new Vector3(row, 0, col), Quaternion.identity);
+                GameObject ob = GetRandomRock();
+                Instantiate (ob, new Vector3(row, 0, col), Quaternion.identity);
                 walkableTiles[row, col] = false;
                 return Scenary.Rock;
             }
@@ -1037,25 +1159,249 @@ namespace Ecosystem.Grid
             return Scenary.Empty;
         }
 
-        private Scenary RandomizeDesert(float value, int row, int col) 
+        private Scenary RandomizeDesertScenary(float value, int row, int col) 
         {
             float newRock = value / 2;
             float rand = Random.value;
             
             if (rand <= newRock)
             {
-                Instantiate (rock, new Vector3(row, 0, col), Quaternion.identity);
+                GameObject ob = GetRandomRock();
+                Instantiate (ob, new Vector3(row, 0, col), Quaternion.identity);
                 walkableTiles[row, col] = false;
                 return Scenary.Rock;
             }
             else if (rand <= value)
             {
-                Instantiate (cactus, new Vector3(row, 0, col), Quaternion.identity);
+                Instantiate (cactus1, new Vector3(row, 0, col), Quaternion.identity);
                 walkableTiles[row, col] = false;
                 return Scenary.Cactus;
             }
             
             return Scenary.Empty;
+        }
+
+        private GameObject GetRandomBush()
+        {
+            float rand = Random.value;
+        
+            if (rand <= 1f/9)
+            {
+                return bush1;
+            }
+            else if (rand <= 2f/9) 
+            {
+                return bush2;
+            }
+            else if (rand <= 3f/9) 
+            {
+                return bush3;
+            }
+            else if (rand <= 4f/9) 
+            {
+                return bush4;
+            }
+            else if (rand <= 5f/9) 
+            {
+                return bush5;
+            }
+            else if (rand <= 6f/9) 
+            {
+                return bush6;
+            }
+            else if (rand <= 7f/9) 
+            {
+                return bush7;
+            }
+            else if (rand <= 8f/9) 
+            {
+                return bush8;
+            }
+            else 
+            {
+                return bush9;
+            }
+        }
+
+        private GameObject GetRandomTree()
+        {
+            float rand = Random.value;
+
+            if (rand <= 1f/30)
+            {
+                return tree1;
+            }
+            else if (rand <= 2f/30) 
+            {
+                return tree2;
+            }
+            else if (rand <= 3f/30) 
+            {
+                return tree3;
+            }
+            else if (rand <= 4f/30) 
+            {
+                return tree4;
+            }
+            else if (rand <= 5f/30) 
+            {
+                return tree5;
+            }
+            else if (rand <= 6f/30) 
+            {
+                return tree6;
+            }
+            else if (rand <= 7f/30) 
+            {
+                return tree7;
+            }
+            else if (rand <= 8f/30) 
+            {
+                return tree8;
+            }
+            else if (rand <= 9f/30) 
+            {
+                return tree9;
+            }
+            else if (rand <= 10f/30) 
+            {
+                return tree10;
+            }
+            else if (rand <= 11f/30) 
+            {
+                return tree11;
+            }
+            else if (rand <= 12f/30) 
+            {
+                return tree12;
+            }
+            else if (rand <= 13f/30)
+            {
+                return tree13;
+            }
+            else if (rand <= 14f/30) 
+            {
+                return tree14;
+            }
+            else if (rand <= 15f/30) 
+            {
+                return tree15;
+            }
+            else if (rand <= 16f/30) 
+            {
+                return tree16;
+            }
+            else if (rand <= 17f/30) 
+            {
+                return tree17;
+            }
+            else if (rand <= 18f/30) 
+            {
+                return tree18;
+            }
+            else if (rand <= 19f/30) 
+            {
+                return tree19;
+            }
+            else if (rand <= 20f/30) 
+            {
+                return tree20;
+            }
+            else if (rand <= 21f/30) 
+            {
+                return tree21;
+            }
+            else if (rand <= 22f/30) 
+            {
+                return tree22;
+            }
+            else if (rand <= 23f/30) 
+            {
+                return tree23;
+            }
+            else if (rand <= 24f/30)
+            {
+                return tree24;
+            }
+            else if (rand <= 25f/30) 
+            {
+                return tree25;
+            }
+            else if (rand <= 26f/30) 
+            {
+                return tree26;
+            }
+            else if (rand <= 27f/30) 
+            {
+                return tree27;
+            }
+            else if (rand <= 28f/30) 
+            {
+                return tree28;
+            }
+            else if (rand <= 29f/30) 
+            {
+                return tree29;
+            }
+            else
+            {
+                return tree30;
+            }
+           
+        }
+
+        private GameObject GetRandomRock()
+        {
+           float rand = Random.value;
+        
+            if (rand <= 1f/12)
+            {
+                return rock1;
+            }
+            else if (rand <= 2f/12) 
+            {
+                return rock2;
+            }
+            else if (rand <= 3f/12) 
+            {
+                return rock3;
+            }
+            else if (rand <= 4f/12) 
+            {
+                return rock4;
+            }
+            else if (rand <= 5f/12) 
+            {
+                return rock5;
+            }
+            else if (rand <= 6f/12) 
+            {
+                return rock6;
+            }
+            else if (rand <= 7f/12) 
+            {
+                return rock7;
+            }
+            else if (rand <= 8f/12) 
+            {
+                return rock8;
+            }
+            else if (rand <= 9f/12)
+            {
+                return rock9;
+            }
+            else if (rand <= 10f/12) 
+            {
+                return rock10;
+            }
+            else if (rand <= 11f/12) 
+            {
+                return rock11;
+            }
+            else 
+            {
+                return rock12;
+            }
         }
     }
 }
