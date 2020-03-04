531f4d05a708ed0a4b3e017acd23828a8228d13b
Anders Magnusson <anders14magnusson@gmail.com>
2020-03-04 20:28:45 +0100

    Optimize freeze rotation system
    Now the system doesn't keep running on entities that have already had
    their rotation frozen.

---
+++ Assets/Scripts/ECS/Physics/FrozenRotation.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+
+namespace Ecosystem.ECS.Physics
+{
+    /// <summary>
+    /// Physics rotation has been frozen for this entity.
+    /// </summary>
+    public struct FrozenRotation : IComponentData
+    {
+    }
+}
