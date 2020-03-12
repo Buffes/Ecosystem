996d72aa5816341aa0a6d2e62098d41462b769a7
Edvin Leidö <14072276+Buffes@users.noreply.github.com>
2020-03-12 18:58:01 +0100

    Moved vision intersection check to a static class Intersections

--- Assets/Scripts/ECS/Targeting/FindSystems/FindFoodSystem.cs
+++ Assets/Scripts/ECS/Targeting/FindSystems/FindFoodSystem.cs
@@ -58,8 +58,8 @@ namespace Ecosystem.ECS.Targeting
                     FoodTypeData targetFoodType = foodTypes[i];
                     float3 targetPosition = positions[i].Value;
                     float targetDistance = math.distance(targetPosition, position.Value);
-                    
-                    if (targetDistance > hearing.Range && !IntersectsVision(targetPosition, position.Value, rotation.Value, vision))
+
+                    if (targetDistance > hearing.Range && !Intersections.IntersectsVision(targetPosition, position.Value, rotation.Value, vision))
                     {
                         continue; // Out of hearing and vision range    
                     } 
@@ -104,30 +104,6 @@ namespace Ecosystem.ECS.Targeting
             }
 
             return false;
-        }
-
-        /// <summary>
-        /// Checks intersection between a point at targetPosition and a circle sector defined by the vision parameter.
-        /// </summary>
-        /// <param name="targetPosition"> The position of the target. </param>
-        /// <param name="position"> The position value of the entity. </param>
-        /// <param name="rotation"> The rotation value of the entity. </param>
-        /// <param name="vision"> The field of vision of the entity, defined by a range and an angle. </param>
-        /// <returns> True if targetPosition intersects the circle sector defined by vision, otherwise false. </returns>
-        private static bool IntersectsVision(float3 targetPosition, float3 position, quaternion rotation, Vision vision)
-        {
-            float3 relativePosition = targetPosition - position;
-
-            if (math.length(relativePosition) > vision.Range) {
-                return false; // Target outside range
-            }
-            relativePosition = math.normalize(relativePosition);
-            float3 forward = math.normalize(math.forward(rotation));
-            float forwardAngle = math.atan2(forward.z, forward.x);
-            
-            float targetAngle = math.atan2(relativePosition.z, relativePosition.x);
-            bool intersects = math.abs(targetAngle - forwardAngle) < vision.Angle;
-            return intersects;
         }
     }
 }
