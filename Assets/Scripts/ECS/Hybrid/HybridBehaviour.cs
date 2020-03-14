1fab7208ec5cd851ce68bb4eff538f3ea4c3a47c
Anders Magnusson <anders14magnusson@gmail.com>
2020-03-14 16:44:30 +0100

    Add HybridBehaviour

---
+++ Assets/Scripts/ECS/Hybrid/HybridBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Ecosystem.ECS.Hybrid
+{
+    [RequireComponent(typeof(ConvertToEntity))]
+    public class HybridBehaviour : MonoBehaviour, IConvertGameObjectToEntity
+    {
+        public Action Converted;
+        public bool HasConverted { get; private set; }
+        protected Entity Entity
+        {
+            get
+            {
+                if (!HasConverted) throw notConvertedException;
+                return _entity;
+            }
+        }
+        protected EntityManager EntityManager
+        {
+            get
+            {
+                if (!HasConverted) throw notConvertedException;
+                return _entityManager;
+            }
+        }
+
+        private Entity _entity;
+        private EntityManager _entityManager;
+        private Exception notConvertedException => new InvalidOperationException("The entity has not been converted yet."
+            + " You should listen to the Converted event or check the HasConverted bool");
+
+        public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+        {
+            _entity = entity;
+            _entityManager = dstManager;
+
+            HasConverted = true;
+            Converted();
+        }
+    }
+}
