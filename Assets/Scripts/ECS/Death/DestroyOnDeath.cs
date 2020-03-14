152f508125c81d8ad1e971151b099c47f664c222
Anders Magnusson <anders14magnusson@gmail.com>
2020-03-14 22:28:41 +0100

    Add destroy on death system

---
+++ Assets/Scripts/ECS/Death/DestroyOnDeath.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Ecosystem.ECS.Death
+{
+    /// <summary>
+    /// Destroys an accompanying game object on death.
+    /// </summary>
+    [GenerateAuthoringComponent]
+    public class DestroyOnDeath : IComponentData
+    {
+        public Object Destroy;
+    }
+}
