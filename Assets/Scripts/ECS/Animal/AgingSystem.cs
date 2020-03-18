772f1d5f6ef8292a731219360147c05beedcab91
Edvin Leidö <14072276+Buffes@users.noreply.github.com>
2020-03-18 21:41:19 +0100

    Added an age component and aging system

---
+++ Assets/Scripts/ECS/Animal/AgingSystem.cs
@@ -0,0 +1,24 @@
+using Unity.Entities;
+
+
+namespace Ecosystem.ECS.Animal
+{
+    /// <summary>
+    /// Increases the age of all animals.
+    /// </summary>
+    public class AgingSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            float deltaTime = Time.DeltaTime;
+
+            Entities.ForEach((Entity entity, int entityInQueryIndex,
+            ref AgeData age) =>
+            {
+                // Store age in seconds.
+                age.Age += deltaTime / 1000f;
+
+            }).ScheduleParallel();
+        }
+    }
+}
