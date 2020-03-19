76376d3dc35d02f80620df91f8c756fed6dfdb75
emmanir <44803542+emmanir@users.noreply.github.com>
2020-03-19 16:22:43 +0100

    tree17 and bush1 spawns fruit

--- Assets/Samples/Sample_berries/Scripts/Spawner.cs
+++ Assets/Samples/Sample_berries/Scripts/Spawner.cs
@@ -1,35 +1,40 @@
 ﻿using UnityEngine;
 
-namespace Ecosystem.Berries {
+namespace Ecosystem.Eatable {
     public class Spawner : MonoBehaviour {
 
         [SerializeField]
         private Transform parent = default;
         [SerializeField]
-        private GameObject berryPrefab = default;
+        private GameObject fruitPrefab = default;
         [SerializeField]
         [Range(1,10)]
         private int spawnAmount = 1;
+        private float deltaTime = 0f;
+        [SerializeField]
+        private float spawnTime = 2f;
 
         [Header("Random Spawn Area")]
 
         [SerializeField]
-        private int startX = 0;
-        [SerializeField]
-        private int endX = 100;
-        [SerializeField]
-        private int startZ = 0;
-        [SerializeField]
-        private int endZ = 100;
+        private float range = 1.5f;
 
-        public void SpawnBerries() {
-            SpawnPrefabs(berryPrefab);
+        public void SpawnFruits() {
+            SpawnPrefabs(fruitPrefab);
         }
 
         private void SpawnPrefabs(GameObject prefab) {
             for (int i = 0; i < spawnAmount; i++) {
                 GameObject o = Instantiate(prefab) as GameObject;
-                o.transform.position = parent.transform.position + new Vector3(Random.Range(startX,endX),parent.transform.position.y-1.5f,Random.Range(startZ,endZ));
+                o.transform.position = parent.transform.position + new Vector3(Random.Range(-range,range),parent.transform.position.y,Random.Range(-range,range));
+            }
+        }
+
+        private void Update() {
+            deltaTime += Time.deltaTime;
+            if (deltaTime > spawnTime) {
+                deltaTime = 0f;
+                SpawnFruits();
             }
         }
     }
