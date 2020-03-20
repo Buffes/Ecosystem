d7beaabcc7a7adb55111ee516173af828910021b
Anders Magnusson <anders14magnusson@gmail.com>
2020-03-20 09:53:24 +0100

    Add a base stats + modifiers system

---
+++ Assets/Scripts/ECS/Movement/Sprinting.cs
@@ -0,0 +1,23 @@
+﻿using Ecosystem.ECS.Stats;
+using Unity.Entities;
+
+namespace Ecosystem.ECS.Movement
+{
+    [GenerateAuthoringComponent]
+    public struct Sprinting : IComponentData
+    {
+    }
+
+    public class BuffSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            Entities
+                .WithAll<Sprinting>()
+                .ForEach((DynamicBuffer<SpeedBuff> speedBuffs) =>
+                {
+                    speedBuffs.Add(new SpeedBuff { Multiplier = 2 });
+                }).ScheduleParallel();
+        }
+    }
+}
---
+++ Assets/Scripts/ECS/Stats/Base/BaseStats/BaseSpeed.cs
@@ -0,0 +1,22 @@
+﻿using Ecosystem.ECS.Movement;
+using Unity.Entities;
+
+namespace Ecosystem.ECS.Stats.Base
+{
+    [GenerateAuthoringComponent]
+    public struct BaseSpeed : IComponentData, BaseStat
+    {
+        public float Value;
+
+        public float BaseValue => Value;
+    }
+
+    public class BaseSpeedSystem : BaseStatSystem
+    {
+        protected override void OnUpdate()
+        {
+            Entities.ForEach((ref MovementSpeed movementspeed, in BaseSpeed baseSpeed)
+                => ResetToBase(ref movementspeed.Value, baseSpeed)).ScheduleParallel();
+        }
+    }
+}
