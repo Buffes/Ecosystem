39fae8bfd0b517ba6b803bb7dff34cf0d8a729ee
Slarvsylt <42959581+Slarvsylt@users.noreply.github.com>
2020-03-26 15:54:48 +0100

    Added a component for prefabs

---
+++ Assets/Scripts/ECS/Animal/AnimalPrefab.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Ecosystem.ECS.Animal
+{
+    [GenerateAuthoringComponent]
+    public struct AnimalPrefab : IComponentData
+    {
+        public Entity EntityPrefab;
+    }
+}
--- Assets/Scripts/ECS/Reproduction/BirthSystem.cs
+++ Assets/Scripts/ECS/Reproduction/BirthSystem.cs
@@ -1,5 +1,6 @@
 ﻿using Unity.Entities;
 using Ecosystem.ECS.Animal;
+using Ecosystem.Attributes;
 using Ecosystem.Genetics;
 
 namespace Ecosystem.ECS.Reproduction
@@ -21,16 +22,17 @@ namespace Ecosystem.ECS.Reproduction
         protected override void OnUpdate()
         {
             var commandBuffer = m_EndSimulationEcbSystem.CreateCommandBuffer().ToConcurrent();
-            var entityManager = m_EndSimulationEcbSystem.EntityManager;
 
             Entities.WithAll<BirthEvent>().ForEach((Entity entity, int entityInQueryIndex
-                , ref PregnancyData pregnancyData) =>
+                , ref PregnancyData pregnancyData
+                , in DNA dna
+                , in AnimalPrefab prefab) =>
             {
                 //TODO Spawn new animal of the same type as parents
 
-                DNA newDNA = DNA.InheritedDNA(EntityManager.GetComponentData<DNA>(entity), pregnancyData.DNAfromFather); // inherit genes from parents
-                
-                
+                DNA newDNA = DNA.InheritedDNA(dna, pregnancyData.DNAfromFather); // inherit genes from parents
+
+
             }).ScheduleParallel();
 
             m_EndSimulationEcbSystem.AddJobHandleForProducer(Dependency);
