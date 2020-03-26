16d1dd94b019b39cee365df0c5f6e0cb1d182d16
Slarvsylt <42959581+Slarvsylt@users.noreply.github.com>
2020-03-26 17:53:39 +0100

    Progress(?)

--- Assets/Scripts/ECS/Animal/AnimalPrefab.cs
+++ Assets/Scripts/ECS/Animal/AnimalPrefab.cs
@@ -6,6 +6,6 @@ namespace Ecosystem.ECS.Animal
     [GenerateAuthoringComponent]
     public struct AnimalPrefab : IComponentData
     {
-        public Entity EntityPrefab;
+        public GameObject Prefab;
     }
 }
--- Assets/Scripts/ECS/Reproduction/BirthSystem.cs
+++ Assets/Scripts/ECS/Reproduction/BirthSystem.cs
@@ -2,6 +2,8 @@
 using Ecosystem.ECS.Animal;
 using Ecosystem.Attributes;
 using Ecosystem.Genetics;
+using Unity.Transforms;
+using UnityEngine;
 
 namespace Ecosystem.ECS.Reproduction
 {
@@ -12,11 +14,15 @@ namespace Ecosystem.ECS.Reproduction
     {
         EndSimulationEntityCommandBufferSystem m_EndSimulationEcbSystem;
 
+        private EntityQuery query;
+
         protected override void OnCreate()
         {
             base.OnCreate();
             m_EndSimulationEcbSystem = World
                 .GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+
+            query = GetEntityQuery(ComponentType.ReadOnly<EntityArchetype>());
         }
 
         protected override void OnUpdate()
@@ -25,13 +31,17 @@ namespace Ecosystem.ECS.Reproduction
 
             Entities.WithAll<BirthEvent>().ForEach((Entity entity, int entityInQueryIndex
                 , ref PregnancyData pregnancyData
+                , in Translation position
+                , in Rotation rotation
                 , in DNA dna
                 , in AnimalPrefab prefab) =>
             {
                 //TODO Spawn new animal of the same type as parents
-
                 DNA newDNA = DNA.InheritedDNA(dna, pregnancyData.DNAfromFather); // inherit genes from parents
-
+                //Entity baby = new Entity();
+                Attributes.Animal baby = new Attributes.Animal();
+                baby.InitDNA(newDNA);
+                GameObject babyObject = Object.Instantiate(prefab.Prefab, position.Value, rotation.Value);
 
             }).ScheduleParallel();
 
