0172a5fb6ab251d8b6848aa5fb4a5e8834191053
Kasper Hall <kasperhall@hotmail.com>
2020-03-27 18:08:45 +0100

    fixed bug

--- Assets/Scripts/Spawner/ResourceReg.cs
+++ Assets/Scripts/Spawner/ResourceReg.cs
@@ -16,6 +16,7 @@ public class ResourceReg : MonoBehaviour
     public GameObject grass;
     public float GrassSpawnTime;
     private float deltaTimeGrass = 0f;
+    private float searchTime = 0f;
 
     private bool lookingForFreeTile = true;
     private int gridSize;
@@ -24,69 +25,85 @@ public class ResourceReg : MonoBehaviour
     public float SeaweedSpawnTime;
     private float deltaTimeSeaweed = 0f;
 
-    private List<int> occupiedWaterSpace = new List<int>();
+    public static List<int> occupiedWaterSpace = new List<int>();
 
- 
+    private NativeArray<bool> grid;
+    private int[,] tiles;
+
     void Update()
     {
-        int[,] tiles = GameZone.tiles;
-        NativeArray<bool> grid = GameZone.walkableTiles;
-        gridSize = grid.Length;
+        
         deltaTimeGrass += Time.deltaTime;
         deltaTimeSeaweed += Time.deltaTime;
+        
+        
+        //bool yesboi;
+
         if (deltaTimeGrass >= GrassSpawnTime)
         {
+            tiles = GameZone.tiles;
+            grid = GameZone.walkableTiles;
+            gridSize = grid.Length;
             RegenerateGrass(grid, tiles);
             deltaTimeGrass = 0f;
         }
 
-        
         if(deltaTimeSeaweed >= SeaweedSpawnTime)
         {
+            tiles = GameZone.tiles;
+            grid = GameZone.walkableTiles;
+            gridSize = grid.Length;
             RegenerateSeaweed(grid, tiles);
-            deltaTimeSeaweed = 0f;
+            deltaTimeSeaweed = 0f;    
         }  
     }
 
     private void RegenerateGrass(NativeArray<bool> grid, int[,] tiles)
     {
-        GameObject o = Instantiate(grass) as GameObject;
         lookingForFreeTile = true;
+        searchTime = 0f;
+        
         while (lookingForFreeTile)
         {
+            if (searchTime > 100f)
+                break;
+            
             int n = UnityEngine.Random.Range(0, gridSize);
             int x = n % GameZone.tiles.GetLength(0);
             int y = (n - x) / GameZone.tiles.GetLength(0);
             if (grid[n] && tiles[x,y] == 51)
             {
+                GameObject o = Instantiate(grass) as GameObject;
                 o.transform.position = new Vector3(x, 1, y);
                 grid[n] = false;
                 lookingForFreeTile = false;
             }
+            searchTime += Time.deltaTime;
         }
     }
 
     private void RegenerateSeaweed(NativeArray<bool> grid, int[,] tiles)
-    {
-        GameObject o = Instantiate(seaweed) as GameObject;
+    { 
         lookingForFreeTile = true;
-        
+        searchTime = 0f;
+
         while (lookingForFreeTile)
         {
+            if(searchTime > 100f) // To not get stuck when there's no tiles left we check ~700 tiles if they are a valid spawnpoint, if no spawnpoint is found we skip the spawning this time            {
+                break;
+            
             int n = UnityEngine.Random.Range(0, gridSize);
             int x = n % GameZone.tiles.GetLength(0);
             int y = (n - x) / GameZone.tiles.GetLength(0);
-           
 
             if (!grid[n] && tiles[x,y] < 34  && !occupiedWaterSpace.Contains(n))
             {
+                GameObject o = Instantiate(seaweed) as GameObject;
                 o.transform.position = new Vector3(x, 1, y);
                 lookingForFreeTile = false;
                 occupiedWaterSpace.Add(n);
-               
             }
+            searchTime += Time.deltaTime;
         }
     }
-    
-
 }
