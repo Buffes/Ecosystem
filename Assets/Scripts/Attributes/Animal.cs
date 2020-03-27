9c1d2de5a258264b87d2ab773a480b8a6f48540a
SemlanChalmers <31894663+SemlanChalmers@users.noreply.github.com>
2020-03-27 10:09:21 +0100

    Visual Debugging started

--- Assets/Scripts/Attributes/Animal.cs
+++ Assets/Scripts/Attributes/Animal.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using Ecosystem.StateMachines;
 using Ecosystem.ECS.Hybrid;
 using Ecosystem.ECS.Animal;
@@ -7,6 +9,17 @@ using Ecosystem.Genetics;
 namespace Ecosystem.Attributes
 {
     public class Animal : MonoBehaviour {
+
+        public float viewRadius;
+        [Range(0,360)]
+        public float viewAngle;
+
+        public LayerMask targetMask;
+        public LayerMask obsicleMask;
+
+        [HideInInspector]
+        public List<Transform> visibleTargets = new List<Transform>();
+
 
         private float hunger;
         private float hungerLimit;
@@ -37,6 +50,7 @@ namespace Ecosystem.Attributes
             this.stateMachine = new StateMachine();
 
             hybridEntity.Converted += Init;
+            StartCoroutine("FindTargetsWithDelay", .2f);
         }
 
         private void OnDestroy()
@@ -151,7 +165,36 @@ namespace Ecosystem.Attributes
             return this.movement;
         }
 
+        public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal) {
+            if (!angleIsGlobal) {
+                angleInDegrees += transform.eulerAngles.y;
+            }
+            return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+        }
 
+        public void FindVisibleTargets() {
+            visibleTargets.Clear();
+            Collider[] targetInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+            for (int i = 0; i < targetInViewRadius.Length; i++) {
+                Transform target = targetInViewRadius[i].transform;
+                Vector3 dirToTarget = (target.position - transform.position).normalized;
+                   
+                if (Vector3.Angle (transform.forward, dirToTarget) < viewAngle / 2) {
+                    float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                    if (!Physics.Raycast(transform.position, dirToTarget, distanceToTarget, obsicleMask)) {
+                        visibleTargets.Add(target);
+                    }
+                }
+            }
+        }
+
+        IEnumerator FindTargetsWithDelay(float delay) {
+            while (true) {
+                yield return new WaitForSeconds(delay);
+                FindVisibleTargets();            }
+        }
 
     }
 }
---
+++ Assets/Scripts/ECS/Debugging/VisualDebugging/FieldOfView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor (typeof (Ecosystem.Attributes.Animal))]
+public class FieldOfView : Editor
+{
+    private void OnSceneGUI() {
+        Ecosystem.Attributes.Animal animal = (Ecosystem.Attributes.Animal)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(animal.transform.position, Vector3.up, Vector3.forward, 360, animal.viewRadius);
+        Vector3 viewAngleA = animal.DirFromAngle(-animal.viewAngle / 2, false);
+        Vector3 viewAngleB = animal.DirFromAngle(animal.viewAngle / 2, false);
+
+        Handles.DrawLine(animal.transform.position, animal.transform.position + viewAngleA * animal.viewRadius);
+        Handles.DrawLine(animal.transform.position, animal.transform.position + viewAngleB * animal.viewRadius);
+
+        Handles.color = Color.red;
+        foreach (Transform visibleTarget in animal.visibleTargets) {
+            Handles.DrawLine(animal.transform.position, visibleTarget.position);
+        }
+
+    }
+}
