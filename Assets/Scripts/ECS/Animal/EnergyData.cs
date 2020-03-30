29eb535288eabf68a258145e78f9031a2d507cbc
emmanir <44803542+emmanir@users.noreply.github.com>
2020-03-30 12:09:51 +0200

    Implemented Energy-loss for animals who are sprinting

---
+++ Assets/Scripts/ECS/Animal/EnergyData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Unity.Entities;
+
+namespace Ecosystem.ECS.Animal {
+    /// <summary>
+    /// A float value ranging from 0 to infinity indicating the level of energy. Energy <= 0 == death?
+    /// </summary>
+    [Serializable]
+    [GenerateAuthoringComponent]
+    public struct EnergyData : IComponentData {
+        public float Energy;
+    }
+}
---
+++ Assets/Scripts/ECS/Animal/EnergySystem.cs
@@ -0,0 +1,37 @@
+﻿using Unity.Entities;
+using Ecosystem.ECS.Death;
+using Ecosystem.ECS.Movement;
+
+namespace Ecosystem.ECS.Animal {
+    /// <summary>
+    /// System for increasing the energy of an animal.
+    /// </summary>
+    public class EnergySystem : SystemBase {
+        private EndSimulationEntityCommandBufferSystem m_EndSimulationEcbSystem;
+
+        protected override void OnCreate() {
+            m_EndSimulationEcbSystem = World
+                .GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+        }
+
+        protected override void OnUpdate() {
+            var commandBuffer = m_EndSimulationEcbSystem.CreateCommandBuffer().ToConcurrent();
+
+            float deltaTime = Time.DeltaTime;
+
+            Entities.ForEach((Entity entity,int entityInQueryIndex,
+                ref EnergyData energyData) => {
+                    if (EntityManager.HasComponent<Sprinting>(entity)) {
+                        energyData.Energy -= deltaTime / 100.0f;
+                    }
+                    
+                    if (energyData.Energy <= 0.0f) {
+                        commandBuffer.AddComponent<DeathEvent>(entityInQueryIndex,entity);
+                    }
+
+                }).ScheduleParallel();
+
+            m_EndSimulationEcbSystem.AddJobHandleForProducer(Dependency);
+        }
+    }
+}
