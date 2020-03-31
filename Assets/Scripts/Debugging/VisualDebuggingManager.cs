6b1ad253a522ab8faf0e5d9d3d9a45b506ec7af0
Slarvsylt <42959581+Slarvsylt@users.noreply.github.com>
2020-03-31 10:49:25 +0200

    Added a shader for the status bars and started on the system

--- Assets/Scripts/Debugging/VisualDebuggingManager.cs
+++ Assets/Scripts/Debugging/VisualDebuggingManager.cs
@@ -14,6 +14,7 @@ namespace Ecosystem.Debugging
         [SerializeField] private bool hearingDebugShow = default;
         [SerializeField] private bool pathDebugShow = default;
         [SerializeField] private bool stateDebugShow = default;
+        [SerializeField] private bool statusDebugShow = default;
 
         [Header("Hearing")]
         [SerializeField] private Material hearingMaterial = default;
@@ -24,7 +25,7 @@ namespace Ecosystem.Debugging
         [Header("State")]
         [SerializeField] private Material stateMaterial = default;
         [SerializeField] private float stateRadius = 0.15f;
-        [SerializeField] private float stateHeight = 2.5f;
+        [SerializeField] private float stateHeight = 3.5f;
         [SerializeField] private Color defaultColor = default;
         [SerializeField] private Color casualColor = default;
         [SerializeField] private Color hungerColor = default;
@@ -32,10 +33,15 @@ namespace Ecosystem.Debugging
         [SerializeField] private Color mateColor = default;
         [SerializeField] private Color fleeColor = default;
 
+        [Header("Status")]
+        [SerializeField] private Material statusMaterial = default;
+        [SerializeField] private float statusHeight = 2.5f;
+
 
         private HearingDebuggingSystem hearingDebuggingSystem;
         private PathDebuggingSystem pathDebuggingSystem;
         private AnimalStateDebugging animalStateDebuggingSystem;
+        private StatusBarsSystem statusBarsSystem;
 
         private void Awake()
         {
@@ -43,6 +49,7 @@ namespace Ecosystem.Debugging
             hearingDebuggingSystem = world.GetOrCreateSystem<HearingDebuggingSystem>();
             pathDebuggingSystem = world.GetOrCreateSystem<PathDebuggingSystem>();
             animalStateDebuggingSystem = world.GetOrCreateSystem<AnimalStateDebugging>();
+            statusBarsSystem = world.GetOrCreateSystem<StatusBarsSystem>();
         }
 
         private void Update()
@@ -63,6 +70,13 @@ namespace Ecosystem.Debugging
             animalStateDebuggingSystem.ThirstColor = thirstColor;
             animalStateDebuggingSystem.MateColor = mateColor;
             animalStateDebuggingSystem.FleeColor = fleeColor;
+
+            statusBarsSystem.Material = statusMaterial;
+            statusBarsSystem.Show = statusDebugShow;
+            statusBarsSystem.Height = statusHeight;
+            statusBarsSystem.HungerColor = hungerColor;
+            statusBarsSystem.ThirstColor = thirstColor;
+            statusBarsSystem.MateColor = mateColor;
         }
     }
 }
--- Assets/Scripts/ECS/Debugging/StatusBarsSystem.cs
+++ Assets/Scripts/ECS/Debugging/StatusBarsSystem.cs
@@ -1,12 +1,88 @@
-﻿using System;
+﻿using Ecosystem.ECS.Debugging.Graphics;
+using Ecosystem.ECS.Debugging.Selection;
+using Ecosystem.ECS.Targeting.Sensors;
+using Ecosystem.ECS.Animal.Needs;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+using Unity.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Unity.Mathematics;
 
 namespace Ecosystem.ECS.Debugging
 {
-    class StatusBarsSystem
+    public class StatusBarsSystem : SystemBase
     {
+        Camera mainCamera;
+        public bool Show { get; set; }
+        public Material Material { get; set; }
+        public Color HungerColor { get; set; }
+        public Color ThirstColor { get; set; }
+        public Color MateColor { get; set; }
+        public float Height { get; set; }
+
+        private EndSimulationEntityCommandBufferSystem m_EndSimulationEcbSystem;
+
+        private EntityQuery query;
+
+        private Mesh mesh;
+        private TextMesh bar;
+
+        protected override void OnCreate()
+        {
+            mainCamera = Camera.main;
+
+            m_EndSimulationEcbSystem = World
+                .GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+
+            query = GetEntityQuery(
+                ComponentType.ReadOnly<Selected>(),
+                ComponentType.ReadOnly<Translation>());
+        } 
+
+        protected override void OnUpdate()
+        {
+            if (!Show) return;
+
+            Entities
+                .WithoutBurst()
+                .WithAll<Selected>()
+                .ForEach((Entity entity,
+                    in HungerData hungerData,
+                    in ThirstData thirstData,
+                    in SexualUrgesData sexualUrgesData) =>
+                {
+
+                }).Run();
+        }
+
+        private void Draw()
+        {
+            var translations = query.ToComponentDataArray<Translation>(Allocator.TempJob);
+
+            MaterialPropertyBlock materialPropertyBlock = new MaterialPropertyBlock();
+            List<Matrix4x4> matrices = new List<Matrix4x4>();
+            //List<Vector4> colors = new List<Vector4>();
+
+            for (int i = 0; i < translations.Length; i++)
+            {
+                float3 position = translations[i].Value;
+                position.y += Height;
+                matrices.Add(Matrix4x4.TRS(position, Quaternion.identity, Vector3.one));
+            }
+
+            //materialPropertyBlock.SetVectorArray("_Colors", colors);
+
+            UnityEngine.Graphics.DrawMeshInstanced(
+                mesh,
+                0,
+                Material,
+                matrices,
+                materialPropertyBlock
+            );
+
+            translations.Dispose(Dependency);
+        }
+
     }
 }
