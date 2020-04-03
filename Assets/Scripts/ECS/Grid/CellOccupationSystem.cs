19b95c31ee9a7063be50a285b72bec7e318c1ed4
Anders Magnusson <anders14magnusson@gmail.com>
2020-04-03 18:46:34 +0200

    Move to namespace

--- Assets/Scripts/ECS/Grid/CellOccupationSystem.cs
+++ Assets/Scripts/ECS/Grid/CellOccupationSystem.cs
@@ -1,63 +1,65 @@
-﻿using Ecosystem.ECS.Grid;
-using Unity.Entities;
+﻿using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 
-/// <summary>
-/// Sets cells in the world as occupied at every position where there is a cell occupant entity,
-/// and restores the cell after the entity is destroyed.
-/// </summary>
-public class CellOccupationSystem : SystemBase
+namespace Ecosystem.ECS.Grid
 {
-    private EndSimulationEntityCommandBufferSystem m_EndSimulationEcbSystem;
-    private WorldGridSystem worldGridSystem;
-
-    protected override void OnCreate()
+    /// <summary>
+    /// Sets cells in the world as occupied at every position where there is a cell occupant entity,
+    /// and restores the cell after the entity is destroyed.
+    /// </summary>
+    public class CellOccupationSystem : SystemBase
     {
-        m_EndSimulationEcbSystem = World
-            .GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-        worldGridSystem = World.GetOrCreateSystem<WorldGridSystem>();
-    }
+        private EndSimulationEntityCommandBufferSystem m_EndSimulationEcbSystem;
+        private WorldGridSystem worldGridSystem;
 
-    protected override void OnUpdate()
-    {
-        var commandBuffer = m_EndSimulationEcbSystem.CreateCommandBuffer().ToConcurrent();
+        protected override void OnCreate()
+        {
+            m_EndSimulationEcbSystem = World
+                .GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+            worldGridSystem = World.GetOrCreateSystem<WorldGridSystem>();
+        }
 
-        var grid = worldGridSystem.Grid;
-        var occupiedCells = worldGridSystem.OccupiedCells;
-        var blockedCells = worldGridSystem.BlockedCells;
+        protected override void OnUpdate()
+        {
+            var commandBuffer = m_EndSimulationEcbSystem.CreateCommandBuffer().ToConcurrent();
 
-        Entities
-            .WithNone<OccupyingCell>()
-            .ForEach((Entity entity, int entityInQueryIndex,
-            in CellOccupant occupant, in Translation position) =>
-            {
-                int2 gridPos = grid.GetGridPosition(position.Value); //gridPosition.Value;
-                int index = grid.GetCellIndex(gridPos);
+            var grid = worldGridSystem.Grid;
+            var occupiedCells = worldGridSystem.OccupiedCells;
+            var blockedCells = worldGridSystem.BlockedCells;
 
-                if (occupiedCells[index]) return; // Already occupied
-
-                occupiedCells[index] = true;
-                blockedCells[index] = occupant.BlocksMovement;
-
-                commandBuffer.AddComponent(entityInQueryIndex, entity, new OccupyingCell
+            Entities
+                .WithNone<OccupyingCell>()
+                .ForEach((Entity entity, int entityInQueryIndex,
+                in CellOccupant occupant, in Translation position) =>
                 {
-                    Value = gridPos
-                });
-            }).ScheduleParallel();
+                    int2 gridPos = grid.GetGridPosition(position.Value); //gridPosition.Value;
+                    int index = grid.GetCellIndex(gridPos);
 
-        Entities
-            .WithNone<CellOccupant>()
-            .ForEach((Entity entity, int entityInQueryIndex,
-            in OccupyingCell occupyingCell) =>
-            {
-                int index = grid.GetCellIndex(occupyingCell.Value);
-                occupiedCells[index] = false;
-                blockedCells[index] = false;
+                    if (occupiedCells[index]) return; // Already occupied
 
-                commandBuffer.RemoveComponent<OccupyingCell>(entityInQueryIndex, entity);
-            }).ScheduleParallel();
+                    occupiedCells[index] = true;
+                    blockedCells[index] = occupant.BlocksMovement;
 
-        m_EndSimulationEcbSystem.AddJobHandleForProducer(Dependency);
+                    commandBuffer.AddComponent(entityInQueryIndex, entity, new OccupyingCell
+                    {
+                        Value = gridPos
+                    });
+                }).ScheduleParallel();
+
+            Entities
+                .WithNone<CellOccupant>()
+                .ForEach((Entity entity, int entityInQueryIndex,
+                in OccupyingCell occupyingCell) =>
+                {
+                    int index = grid.GetCellIndex(occupyingCell.Value);
+                    occupiedCells[index] = false;
+                    blockedCells[index] = false;
+
+                    commandBuffer.RemoveComponent<OccupyingCell>(entityInQueryIndex, entity);
+                }).ScheduleParallel();
+
+            m_EndSimulationEcbSystem.AddJobHandleForProducer(Dependency);
+        }
     }
 }
--- Assets/Scripts/ECS/Grid/WorldGridSystem.cs
+++ Assets/Scripts/ECS/Grid/WorldGridSystem.cs
@@ -1,61 +1,63 @@
-﻿using Ecosystem.ECS.Grid;
-using Unity.Collections;
+﻿using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 
-/// <summary>
-/// Holds info about the world grid.
-/// </summary>
-public class WorldGridSystem : SystemBase
+namespace Ecosystem.ECS.Grid
 {
-    public GridData Grid;
-
-    public NativeArray<bool> OccupiedCells;
-    public NativeArray<bool> BlockedCells;
-    public NativeArray<bool> WaterCells;
-
     /// <summary>
-    /// Sets a cell as occupied.
+    /// Holds info about the world grid.
     /// </summary>
-    public void SetOccupiedCell(int2 gridPos, bool blocksMovement = false)
+    public class WorldGridSystem : SystemBase
     {
-        OccupiedCells[Grid.GetCellIndex(gridPos)] = true;
-        BlockedCells[Grid.GetCellIndex(gridPos)] = blocksMovement;
-    }
+        public GridData Grid;
 
-    /// <summary>
-    /// Sets a cell as water.
-    /// </summary>
-    public void SetWaterCell(int2 gridPos) => WaterCells[Grid.GetCellIndex(gridPos)] = true;
+        public NativeArray<bool> OccupiedCells;
+        public NativeArray<bool> BlockedCells;
+        public NativeArray<bool> WaterCells;
 
-    protected override void OnCreate()
-    {
-        InitGrid(100, 100); // Default grid
-    }
+        /// <summary>
+        /// Sets a cell as occupied.
+        /// </summary>
+        public void SetOccupiedCell(int2 gridPos, bool blocksMovement = false)
+        {
+            OccupiedCells[Grid.GetCellIndex(gridPos)] = true;
+            BlockedCells[Grid.GetCellIndex(gridPos)] = blocksMovement;
+        }
 
-    /// <summary>
-    /// Initializes the grid with amount of columns/rows equal to the specified width/height.
-    /// </summary>
-    public void InitGrid(int width, int height, float cellSize = 1f)
-    {
-        Grid = new GridData(width, height, cellSize);
+        /// <summary>
+        /// Sets a cell as water.
+        /// </summary>
+        public void SetWaterCell(int2 gridPos) => WaterCells[Grid.GetCellIndex(gridPos)] = true;
 
-        if (OccupiedCells.IsCreated) OccupiedCells.Dispose();
-        if (BlockedCells.IsCreated) BlockedCells.Dispose();
-        if (WaterCells.IsCreated) WaterCells.Dispose();
-        OccupiedCells = new NativeArray<bool>(Grid.Length, Allocator.Persistent);
-        BlockedCells = new NativeArray<bool>(Grid.Length, Allocator.Persistent);
-        WaterCells = new NativeArray<bool>(Grid.Length, Allocator.Persistent);
-    }
+        protected override void OnCreate()
+        {
+            InitGrid(100, 100); // Default grid
+        }
 
-    protected override void OnDestroy()
-    {
-        OccupiedCells.Dispose();
-        BlockedCells.Dispose();
-        WaterCells.Dispose();
-    }
+        /// <summary>
+        /// Initializes the grid with amount of columns/rows equal to the specified width/height.
+        /// </summary>
+        public void InitGrid(int width, int height, float cellSize = 1f)
+        {
+            Grid = new GridData(width, height, cellSize);
 
-    protected override void OnUpdate()
-    {
+            if (OccupiedCells.IsCreated) OccupiedCells.Dispose();
+            if (BlockedCells.IsCreated) BlockedCells.Dispose();
+            if (WaterCells.IsCreated) WaterCells.Dispose();
+            OccupiedCells = new NativeArray<bool>(Grid.Length, Allocator.Persistent);
+            BlockedCells = new NativeArray<bool>(Grid.Length, Allocator.Persistent);
+            WaterCells = new NativeArray<bool>(Grid.Length, Allocator.Persistent);
+        }
+
+        protected override void OnDestroy()
+        {
+            OccupiedCells.Dispose();
+            BlockedCells.Dispose();
+            WaterCells.Dispose();
+        }
+
+        protected override void OnUpdate()
+        {
+        }
     }
 }
