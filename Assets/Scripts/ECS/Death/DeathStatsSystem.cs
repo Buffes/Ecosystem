c594f14496f6be0c5d0f57ea2d9ba2d3342537b2
emmanir <44803542+emmanir@users.noreply.github.com>
2020-04-03 15:58:48 +0200

    For debugging purposes

--- Assets/Scripts/ECS/Death/DeathStatsSystem.cs
+++ Assets/Scripts/ECS/Death/DeathStatsSystem.cs
@@ -1,6 +1,8 @@
 ﻿using Unity.Entities;
 using UnityEngine;
 using System.Threading;
+using System;
+using System.IO;
 
 namespace Ecosystem.ECS.Death {
     public class DeathStatsSystem : SystemBase {
@@ -12,6 +14,7 @@ namespace Ecosystem.ECS.Death {
         public int other;
 
         protected override void OnCreate() {
+            Debug.Log("create");
             base.OnCreate();
             hunger = 0;
             thirst = 0;
@@ -20,17 +23,33 @@ namespace Ecosystem.ECS.Death {
             other = 0;
         }
         protected override void OnUpdate() {
+            Debug.Log("update");
 
             Entities.WithoutBurst().ForEach((Entity entity,int entityInQueryIndex,in DeathEvent deathEvent) => {
                 switch(deathEvent.Cause) {
-                    case DeathCause.Hunger: Interlocked.Increment(ref hunger); break;
-                    case DeathCause.Thirst: Interlocked.Increment(ref thirst); break;
-                    case DeathCause.Age: Interlocked.Increment(ref age); break;
-                    case DeathCause.Predators: Interlocked.Increment(ref predator); break;
-                    default: Interlocked.Increment(ref other); break;
+                    case DeathCause.Hunger: Debug.Log("hunger"); Interlocked.Increment(ref hunger); break;
+                    case DeathCause.Thirst: Debug.Log("thirst"); Interlocked.Increment(ref thirst); break;
+                    case DeathCause.Age: Debug.Log("age"); Interlocked.Increment(ref age); break;
+                    case DeathCause.Predators: Debug.Log("predators"); Interlocked.Increment(ref predator); break;
+                    default: Debug.Log("other"); Interlocked.Increment(ref other); break;
                 }
             }).Run();
 
+        }
+
+        protected override void OnDestroy()
+		{
+            base.OnDestroy();
+            Debug.Log("destroy");
+            using (StreamWriter sw = new StreamWriter("DeathCauses.csv"))
+            {
+                sw.WriteLine("Cause,Number");
+                sw.WriteLine("Hunger," + hunger);
+                sw.WriteLine("Thirst," + thirst);
+                sw.WriteLine("Age," + age);
+                sw.WriteLine("Predators," + predator);
+                sw.WriteLine("Other," + other);
+            }
         }
     }
 }
