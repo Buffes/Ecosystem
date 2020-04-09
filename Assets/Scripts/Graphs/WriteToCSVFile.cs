464b8e1a4dbf853c6a9ab20c44c92609d7aae537
Kasper Hall <kasperhall@hotmail.com>
2020-04-09 17:51:40 +0200

    Speed, hearing range and vision range are now plotted aswell

--- Assets/Scripts/Graphs/WriteToCSVFile.cs
+++ Assets/Scripts/Graphs/WriteToCSVFile.cs
@@ -23,22 +23,47 @@ public class WriteToCSVFile : MonoBehaviour
     {
         int nrChickens = 0;
         int nrLions = 0;
+        float lionHearingRange = 0f;
+        float lionVisionRange = 0f;
+        float lionSpeed = 0f;
+        float chickenHearingRange = 0f;
+        float chickenVisionRange = 0f;
+        float chickenSpeed = 0f;
 
-        List<(int, Entity)> list = WriteToCSVSystem.list;
+        List<(int, float, float, float, Entity)> attributeList = WriteToCSVSystem.attributeList;
 
         if (currentTime > sampleTime)
         {
-            for( int i = 0; i < list.Count; i++)
+            for( int i = 0; i < attributeList.Count; i++)
             {
 
-                if(list[i].Item1 == getLionID())
+                if (attributeList[i].Item1 == getLionID())
+                {
                     nrLions++;
-                
-                if (list[i].Item1 == getChickenID())
+                    lionSpeed += attributeList[i].Item2;
+                    lionHearingRange += attributeList[i].Item3;
+                    lionVisionRange += attributeList[i].Item4;
+                }
+
+                if (attributeList[i].Item1 == getChickenID())
+                {
                     nrChickens++;
+                    chickenSpeed += attributeList[i].Item2;
+                    chickenHearingRange += attributeList[i].Item3;
+                    chickenVisionRange += attributeList[i].Item4;
+                }
             }
 
-            addRecord(Mathf.RoundToInt(systemAge).ToString(), nrChickens.ToString(), nrLions.ToString());
+            chickenSpeed = chickenSpeed / nrChickens;
+            chickenHearingRange = chickenHearingRange / nrChickens;
+            chickenVisionRange = chickenVisionRange / nrChickens;
+            lionSpeed = lionSpeed / nrLions;
+            lionHearingRange = lionHearingRange / nrLions;
+            lionVisionRange = lionVisionRange / nrLions;
+
+            addRecord(Mathf.RoundToInt(systemAge).ToString(), nrChickens.ToString(), nrLions.ToString(), Mathf.RoundToInt(chickenSpeed).ToString(),
+                Mathf.RoundToInt(chickenHearingRange).ToString(), Mathf.RoundToInt(chickenVisionRange).ToString(), Mathf.RoundToInt(lionSpeed).ToString(),
+                Mathf.RoundToInt(lionHearingRange).ToString(), Mathf.RoundToInt(lionVisionRange).ToString());
             currentTime = 0f;
         }
         systemAge += Time.deltaTime;
@@ -46,13 +71,14 @@ public class WriteToCSVFile : MonoBehaviour
 
     }
 
-    public static void addRecord(string time, string nrChickens, string nrLions)
+    public static void addRecord(string time, string nrChickens, string nrLions, string chickenSpeed, string chickenHearingRange,
+        string chickenVisionRange, string lionSpeed, string lionHearingRange, string lionVisionRange)
     {
         string path = "/Scripts/Graphs/GraphValues.csv";
 
         StreamWriter file = new StreamWriter(Application.dataPath + path, true);
 
-        var line = String.Format("{0},{1},{2}", time, nrChickens, nrLions);
+        var line = String.Format("{0},{1},{2},{3},{4},{5},{6},{7},{8}", time, nrChickens, nrLions, chickenSpeed, chickenHearingRange, chickenVisionRange, lionSpeed, lionHearingRange, lionVisionRange);
         file.WriteLine(line);
         file.Close();
     }
