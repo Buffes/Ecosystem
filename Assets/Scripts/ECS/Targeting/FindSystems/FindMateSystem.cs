b2e44bd0a98069a8c859e0008eec2a9e347bd311
Anders Magnusson <anders14magnusson@gmail.com>
2020-04-10 03:21:53 +0200

    Update find systems to use the shared sensed entities

--- Assets/Scripts/ECS/Targeting/FindSystems/FindMateSystem.cs
+++ Assets/Scripts/ECS/Targeting/FindSystems/FindMateSystem.cs
@@ -1,8 +1,7 @@
 ﻿using Ecosystem.ECS.Animal;
+using Ecosystem.ECS.Grid.Buckets;
 using Ecosystem.ECS.Movement.Pathfinding;
-using Ecosystem.ECS.Targeting.Sensing;
 using Ecosystem.ECS.Targeting.Targets;
-using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
@@ -14,64 +13,37 @@ namespace Ecosystem.ECS.Targeting.FindSystems
     /// </summary>
     public class FindMateSystem : SystemBase
     {
-        private EndSimulationEntityCommandBufferSystem m_EndSimulationEcbSystem;
-
-        private EntityQuery query;
-
-        protected override void OnCreate()
-        {
-            m_EndSimulationEcbSystem = World
-                .GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
-
-            query = GetEntityQuery(
-                ComponentType.ReadOnly<Translation>(),
-                ComponentType.ReadOnly<AnimalTypeData>(),
-                ComponentType.ReadOnly<SexData>()
-                );
-            
-        }
-
         protected override void OnUpdate()
         {
-            var commandBuffer = m_EndSimulationEcbSystem.CreateCommandBuffer().ToConcurrent();
-
-            var entities = query.ToEntityArray(Allocator.TempJob);
-            var positions = query.ToComponentDataArray<Translation>(Allocator.TempJob);
-            var animalTypes = query.ToComponentDataArray<AnimalTypeData>(Allocator.TempJob);
-            var sexTypes = query.ToComponentDataArray<SexData>(Allocator.TempJob);
-            
-            // Get buffers here since ForEach lambda has max 9 parameters. Should be unnecessary once the Separate concerns in find-systems task is done
-            var unreachableBuffers = GetBufferFromEntity<UnreachablePosition>(true);
+            var sexTypes = GetComponentDataFromEntity<SexData>(true);
 
             Entities
-                .WithReadOnly(entities)
-                .WithReadOnly(positions)
-                .WithReadOnly(animalTypes)
                 .WithReadOnly(sexTypes)
-                .WithReadOnly(unreachableBuffers)
-                .ForEach((Entity entity, int entityInQueryIndex,
+                .ForEach((
                 ref LookingForMate lookingForMate,
                 in Translation position,
-                in Hearing hearing,
                 in AnimalTypeData animalType,
-                in SexData sexType) =>
+                in SexData sexType,
+                in DynamicBuffer<BucketAnimalData> sensedAnimals,
+                in DynamicBuffer<UnreachablePosition> unreachablePositions) =>
                 {
                     
                     int closestMateIndex = -1;
                     float closestMateDistance = 0f;
 
-                    for (int i = 0; i < entities.Length; i++)
+                    for (int i = 0; i < sensedAnimals.Length; i++)
                     {
-                        AnimalTypeData targetAnimalType = animalTypes[i];
-                        SexData targetSexType = sexTypes[i];
-                        float3 targetPosition = positions[i].Value;
+                        var sensedAnimalInfo = sensedAnimals[i];
+
+                        AnimalTypeData targetAnimalType = sensedAnimalInfo.AnimalTypeData;
+                        SexData targetSexType = sexTypes[sensedAnimalInfo.Entity];
+                        float3 targetPosition = sensedAnimalInfo.Position;
                         float targetDistance = math.distance(targetPosition, position.Value);
 
-                        if (targetDistance > hearing.Range) continue; // Out of range
-                        if (animalType.AnimalTypeId != targetAnimalType.AnimalTypeId) continue; //If not the same type of animal
+                        if (animalType.AnimalTypeId != targetAnimalType.AnimalTypeId) continue; // Not the same type of animal
                         if (closestMateIndex != -1 && targetDistance >= closestMateDistance) continue; // Not the closest
-                        if (sexType.Sex != targetSexType.Sex)   continue; // If the same sex
-                        if (Utilities.IsUnreachable(unreachableBuffers[entity], targetPosition)) continue;
+                        if (sexType.Sex != targetSexType.Sex) continue; // Not the opposite sex
+                        if (Utilities.IsUnreachable(unreachablePositions, targetPosition)) continue;
 
                         closestMateIndex = i;
                         closestMateDistance = targetDistance;
@@ -81,23 +53,15 @@ namespace Ecosystem.ECS.Targeting.FindSystems
                     if (closestMateIndex != -1)
                     {
                         lookingForMate.HasFound = true;
-                        lookingForMate.Entity = entities[closestMateIndex];
-                        lookingForMate.Position = positions[closestMateIndex].Value;
+                        lookingForMate.Entity = sensedAnimals[closestMateIndex].Entity;
+                        lookingForMate.Position = sensedAnimals[closestMateIndex].Position;
                     }
                     else
                     {
                         lookingForMate.HasFound = false;
                     }
 
-
                 }).ScheduleParallel();
-
-            entities.Dispose(Dependency);
-            positions.Dispose(Dependency);
-            animalTypes.Dispose(Dependency);
-            sexTypes.Dispose(Dependency);
-
-            m_EndSimulationEcbSystem.AddJobHandleForProducer(Dependency);
         }
     }
 }
