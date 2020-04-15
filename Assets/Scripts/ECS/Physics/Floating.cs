06b496318b77ce2d8cb3ffe30c58a1fc947355b9
Edvin Leidö <14072276+Buffes@users.noreply.github.com>
2020-04-15 12:50:36 +0200

    Added floating component

---
+++ Assets/Scripts/ECS/Physics/Floating.cs
@@ -0,0 +1,11 @@
+using Unity.Entities;
+
+namespace Ecosystem.ECS.Physics
+{
+    /// <summary>
+    /// If this entity is floating and should not be affected by gravity.
+    /// </summary>
+    public struct Floating : IComponentData
+    {
+    }
+}
