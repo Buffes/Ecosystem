658843f798bf61a83f30a1d2d8145d1ba2c1448e
Slarvsylt <42959581+Slarvsylt@users.noreply.github.com>
2020-04-16 11:23:04 +0200

    Added maxSexualUrgeSystem
    Also updated prefab and StatusBar

---
+++ Assets/Scripts/ECS/Animal/Needs/MaxSexualUrgesSystem.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Entities;
+using Ecosystem.ECS.Death;
+
+namespace Ecosystem.ECS.Animal.Needs
+{
+    /// <summary>
+    /// System for clamping down sexual urge on an animal.
+    /// </summary>
+    public class MaxSexualUrgesSystem : SystemBase
+    {
+
+        protected override void OnUpdate()
+        {
+
+            Entities.ForEach((ref SexualUrgesData urgeData, in MaxSexualUrgesData maxUrgeData) =>
+            {
+                if (urgeData.Urge >= maxUrgeData.MaxUrge)
+                {
+                    urgeData.Urge = maxUrgeData.MaxUrge;
+                }
+
+            }).ScheduleParallel();
+        }
+    }
+}
