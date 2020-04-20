fa657f3a9510a10076d439ec61824e239ddad053
Edvin Leidö <14072276+Buffes@users.noreply.github.com>
2020-04-20 20:45:42 +0200

    debugging

--- Assets/Scripts/ECS/Growth/MaturingSystem.cs
+++ Assets/Scripts/ECS/Growth/MaturingSystem.cs
@@ -1,6 +1,7 @@
 using Ecosystem.ECS.Animal;
 using Unity.Entities;
 using Unity.Transforms;
+using UnityEngine;
 
 namespace Ecosystem.ECS.Growth
 {
@@ -24,6 +25,7 @@ namespace Ecosystem.ECS.Growth
             float deltaTime = Time.DeltaTime/60;
 
             Entities
+                .WithoutBurst()
                 .WithNone<Adult>()
                 .ForEach((Entity entity, int entityInQueryIndex,
                 ref Scale scale,
@@ -31,16 +33,18 @@ namespace Ecosystem.ECS.Growth
                 in LifespanData lifespan) =>
                 {
                     float ageOfMaturity = lifespan.Value * 0.15f; // TODO: Maybe make multiplier dependent on a component. 
-                    
+                    Debug.Log("age: " + age.Age);
+                    Debug.Log("age of maturity: " + ageOfMaturity);
                     if (age.Age < ageOfMaturity)
                     {
                         scale.Value = 0.5f;
+                        Debug.Log("Setting scale to .5");
                     }
                     else
                     {
-                        commandBuffer.AddComponent<Adult>(entityInQueryIndex, entity);
+                        //commandBuffer.AddComponent<Adult>(entityInQueryIndex, entity);
                     }
-                }).ScheduleParallel();
+                }).Run();
 
             m_EndSimulationEcbSystem.AddJobHandleForProducer(Dependency);
         }
