fca98af7d82c82268aab2d9f3738b06cc9cbb160
Edvin Leidö <14072276+Buffes@users.noreply.github.com>
2020-04-20 19:21:18 +0200

    Added maturing system

---
+++ Assets/Scripts/ECS/Growth/Adult.cs
@@ -0,0 +1,12 @@
+using System;
+using Unity.Entities;
+
+namespace Ecosystem.ECS.Animal
+{
+    /// <summary>
+    /// If this is an adult.
+    /// </summary>
+    public struct Adult : IComponentData
+    {
+    }
+}
---
+++ Assets/Scripts/ECS/Growth/MaturingSystem.cs
@@ -0,0 +1,46 @@
+using Ecosystem.ECS.Animal;
+using Unity.Entities;
+
+namespace Ecosystem.ECS.Growth
+{
+    /// <summary>
+    /// Gives young animals lower stats and stops them from mating until maturity.
+    /// </summary>
+    public class MaturingSystem : SystemBase
+    {
+        private EndSimulationEntityCommandBufferSystem m_EndSimulationEcbSystem;
+
+        protected override void OnCreate()
+        {
+            m_EndSimulationEcbSystem = World
+                .GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+        }
+
+        protected override void OnUpdate()
+        {
+            var commandBuffer = m_EndSimulationEcbSystem.CreateCommandBuffer().ToConcurrent();
+
+            float deltaTime = Time.DeltaTime/60;
+
+            Entities
+                .WithNone<Adult>()
+                .ForEach((Entity entity, int entityInQueryIndex,
+                in AgeData age,
+                in LifespanData lifespan) =>
+                {
+                    float ageOfMaturity = lifespan.Value * 0.15f; // TODO: Maybe make multiplier dependent on a component. 
+                    
+                    if (age.Age < ageOfMaturity)
+                    {
+                        
+                    }
+                    else
+                    {
+                        commandBuffer.AddComponent<Adult>(entityInQueryIndex, entity);
+                    }
+                }).ScheduleParallel();
+
+            m_EndSimulationEcbSystem.AddJobHandleForProducer(Dependency);
+        }
+    }
+}
