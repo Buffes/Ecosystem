8ad0a2ff75c374a5469337d3ab141de4b6d87d87
emmanir <44803542+emmanir@users.noreply.github.com>
2020-04-20 10:42:10 +0200

    Add Target and Find systems for fleeing

---
+++ Assets/Scripts/ECS/Targeting/Targets/LookingForFleeTarget.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Ecosystem.ECS.Targeting.Targets
+{
+    [Serializable]
+    [GenerateAuthoringComponent]
+    public struct LookingForFleeTarget : IComponentData
+    {
+        public bool HasFound;
+        public float3 Position;
+        public float3 EnemyPosition;
+    }
+}
