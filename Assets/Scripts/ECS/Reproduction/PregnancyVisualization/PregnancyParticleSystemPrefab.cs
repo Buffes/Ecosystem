27a3da8635735e3c3f2bafc75f54fe3ce64c8e07
Slarvsylt <42959581+Slarvsylt@users.noreply.github.com>
2020-04-21 11:29:23 +0200

    Added a system for visualising pregnancy

---
+++ Assets/Scripts/ECS/Reproduction/PregnancyVisualization/PregnancyParticleSystemPrefab.cs
@@ -0,0 +1,11 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace Ecosystem.ECS.Reproduction.PregrancyVisualization
+{
+    [GenerateAuthoringComponent]
+    public class PregnancyParticleSystemPrefab : IComponentData
+    {
+        public ParticleSystem Prefab;
+    }
+}
---
+++ Assets/Scripts/ECS/Reproduction/PregnancyVisualization/PregnancyVisualizationSystem.cs
@@ -0,0 +1,40 @@
+﻿using Ecosystem.ECS.Animal.Needs;
+using Ecosystem.ECS.Debugging.Selection;
+using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+using Unity.Collections;
+using Unity.Mathematics;
+
+namespace Ecosystem.ECS.Reproduction.PregrancyVisualization
+{
+    public class PregnancyVisualizationSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            Entities
+                .WithoutBurst()
+                .WithAll<PregnancyData>()
+                .ForEach((Entity entity,
+                PregnancyParticleSystemPrefab prefab,
+                in Translation position,
+                in Rotation rotation) =>
+            {
+                if(!prefab.Prefab.IsAlive())
+                    prefab.Prefab.Play();
+            }).Run();
+
+            Entities
+                .WithoutBurst()
+                .WithNone<PregnancyData>()
+                .ForEach((Entity entity,
+                PregnancyParticleSystemPrefab prefab,
+                in Translation position,
+                in Rotation rotation) =>
+                {
+                    if (prefab.Prefab.IsAlive())
+                        prefab.Prefab.Stop();
+                }).Run();
+        }
+    }
+}
