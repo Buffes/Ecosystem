559770602ff088720008a83079cccf558fea8604
Anders Magnusson <anders14magnusson@gmail.com>
2020-04-22 04:49:40 +0200

    Add output text behaviour

---
+++ Assets/Scripts/UI/ChatOutputBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ChatOutputBehaviour : MonoBehaviour
+{
+    [SerializeField] private string messagePrefix = "> ";
+    [SerializeField] private TMP_Text outputText = default;
+
+    public void AddMessage(string message)
+    {
+        string prefix = string.Empty;
+        if (!string.IsNullOrEmpty(outputText.text)) prefix = "\n";
+
+        outputText.text += (prefix + messagePrefix + message);
+    }
+}
