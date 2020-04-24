2f41646785ed8df9b1dd84eb7d03b2b37664397a
emmanir <44803542+emmanir@users.noreply.github.com>
2020-04-24 08:39:41 +0200

    Added infertility

---
+++ Assets/Scripts/ECS/Animal/Infertile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Unity.Entities;
+
+namespace Ecosystem.ECS.Animal
+{
+    /// <summary>
+    /// The animal is infertile.
+    /// </summary>
+    [Serializable]
+    public struct Infertile : IComponentData
+    {
+    }
+}
