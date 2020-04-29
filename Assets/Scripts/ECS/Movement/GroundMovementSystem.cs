4f4fa3f94bccd956ba94ed2e10b34270a5f2c90e
Anders Magnusson <anders14magnusson@gmail.com>
2020-04-29 16:18:30 +0200

    Disable physics for ECS rabbits

--- Assets/Scripts/ECS/Movement/GroundMovementSystem.cs
+++ Assets/Scripts/ECS/Movement/GroundMovementSystem.cs
@@ -12,9 +12,10 @@ public class GroundMovementSystem : SystemBase
 {
     protected override void OnUpdate()
     {
+        var deltaTime = Time.DeltaTime;
         Entities.ForEach((
             ref Rotation rotation,
-            ref PhysicsVelocity velocity,
+            ref Translation position,
             in MovementInput movementInput,
             in MovementStats movementStats) =>
         {
@@ -23,12 +24,13 @@ public class GroundMovementSystem : SystemBase
             float speed = movementInput.Sprint ?
                 movementStats.SprintSpeed : movementStats.Speed;
 
-            velocity.Linear.x = direction.x * speed;
-            velocity.Linear.z = direction.z * speed;
+            //velocity.Linear.x = direction.x * speed;
+            //velocity.Linear.z = direction.z * speed;
             if (math.length(direction) != 0)
             {
                 rotation.Value = quaternion.LookRotation(direction, math.up());
             }
+            position.Value += math.forward(rotation.Value) * 6f * deltaTime;
 
         }).ScheduleParallel();
     }
