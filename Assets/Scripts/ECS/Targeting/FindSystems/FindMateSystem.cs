45af7bf52fad69d84adf8193c545c92f3c455809
emmanir <44803542+emmanir@users.noreply.github.com>
2020-04-29 14:17:43 +0200

    Do an infertile check in FindMateSystem

--- Assets/Scripts/ECS/Targeting/FindSystems/FindMateSystem.cs
+++ Assets/Scripts/ECS/Targeting/FindSystems/FindMateSystem.cs
@@ -24,45 +24,47 @@ namespace Ecosystem.ECS.Targeting.FindSystems
                 .WithReadOnly(adultData)
                 .WithReadOnly(sexTypes)
                 .ForEach((
+                Entity entity,
                 ref LookingForMate lookingForMate,
                 in Translation position,
                 in AnimalTypeData animalType,
                 in SexData sexType,
                 in DynamicBuffer<BucketAnimalData> sensedAnimals) =>
                 {
-                    
-                    int closestMateIndex = -1;
-                    float closestMateDistance = 0f;
 
-                    for (int i = 0; i < sensedAnimals.Length; i++)
-                    {
-                        var sensedAnimalInfo = sensedAnimals[i];
-
-                        AnimalTypeData targetAnimalType = sensedAnimalInfo.AnimalTypeData;
-                        SexData targetSexType = sexTypes[sensedAnimalInfo.Entity];
-                        bool targetAdult = adultData.Exists(sensedAnimalInfo.Entity);
-                        float3 targetPosition = sensedAnimalInfo.Position;
-                        float targetDistance = math.distance(targetPosition, position.Value);
-
-                        if (animalType.AnimalTypeId != targetAnimalType.AnimalTypeId) continue; // Not the same type of animal
-                        if (closestMateIndex != -1 && targetDistance >= closestMateDistance) continue; // Not the closest
-                        if (sexType.Sex == targetSexType.Sex) continue; // Not the opposite sex
-                        if (!targetAdult) continue; // Not an adult
-
-                        closestMateIndex = i;
-                        closestMateDistance = targetDistance;
-                    }
-
-                    // Set result
-                    if (closestMateIndex != -1)
-                    {
-                        lookingForMate.HasFound = true;
-                        lookingForMate.Entity = sensedAnimals[closestMateIndex].Entity;
-                        lookingForMate.Position = sensedAnimals[closestMateIndex].Position;
-                    }
-                    else
-                    {
+                    if (EntityManager.HasComponent<Infertile>(entity)) {
                         lookingForMate.HasFound = false;
+                    } else {
+                        int closestMateIndex = -1;
+                        float closestMateDistance = 0f;
+
+                        for (int i = 0; i < sensedAnimals.Length; i++) {
+                            var sensedAnimalInfo = sensedAnimals[i];
+
+                            AnimalTypeData targetAnimalType = sensedAnimalInfo.AnimalTypeData;
+                            SexData targetSexType = sexTypes[sensedAnimalInfo.Entity];
+                            bool targetAdult = adultData.Exists(sensedAnimalInfo.Entity);
+                            float3 targetPosition = sensedAnimalInfo.Position;
+                            float targetDistance = math.distance(targetPosition,position.Value);
+
+                            if (animalType.AnimalTypeId != targetAnimalType.AnimalTypeId) continue; // Not the same type of animal
+                            if (closestMateIndex != -1 && targetDistance >= closestMateDistance) continue; // Not the closest
+                            if (sexType.Sex == targetSexType.Sex) continue; // Not the opposite sex
+                            if (!targetAdult) continue; // Not an adult
+                            if (EntityManager.HasComponent<Infertile>(sensedAnimalInfo.Entity)) continue; // Target infertile
+
+                            closestMateIndex = i;
+                            closestMateDistance = targetDistance;
+                        }
+
+                        // Set result
+                        if (closestMateIndex != -1) {
+                            lookingForMate.HasFound = true;
+                            lookingForMate.Entity = sensedAnimals[closestMateIndex].Entity;
+                            lookingForMate.Position = sensedAnimals[closestMateIndex].Position;
+                        } else {
+                            lookingForMate.HasFound = false;
+                        }
                     }
 
                 }).ScheduleParallel();
