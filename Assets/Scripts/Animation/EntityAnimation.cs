6007a6a5e92bc01f95b0e7855ea18d5d6f827b99
SemlanChalmers <31894663+SemlanChalmers@users.noreply.github.com>
2020-05-06 09:56:01 +0200

    Animation finished for eagle and sardine

--- Assets/Scripts/Animation/EntityAnimation.cs
+++ Assets/Scripts/Animation/EntityAnimation.cs
@@ -1,12 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Ecosystem.ECS.Hybrid;
+using Unity.Mathematics;
 using UnityEngine;
 
-public class EntityAnimation : MonoBehaviour
-{
-    
-    void Update()
-    {
-        
+namespace Ecosystem.Animation {
+    public class EntityAnimation : MonoBehaviour {
+        private Animator animator;
+
+        [SerializeField]
+        private Movement movement = default;
+
+        void Start() {
+            animator = GetComponent<Animator>();
+        }
+
+        void Update() {
+            if(math.length(movement.GetMovementInput()) != 0) {
+                RunAnimation();
+            } else {
+                IdleAnimation();
+            }
+
+        }
+
+        private void IdleAnimation() {
+            if(animator != null) {
+                animator.SetBool("Run", false);
+            }
+        }
+
+        private void RunAnimation() {
+            if (animator != null) {
+                animator.SetBool("Run", true);
+            }
+        }
     }
 }
+
