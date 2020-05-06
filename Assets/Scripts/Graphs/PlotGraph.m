c65324e1834b50e27a018d1eca52373dfebd1742
Kasper Hall <kasperhall@hotmail.com>
2020-05-06 10:54:14 +0200

    Stops plotting values for extict animals

--- Assets/Scripts/Graphs/PlotGraph.m
+++ Assets/Scripts/Graphs/PlotGraph.m
@@ -4,6 +4,7 @@ file = join(file, "\");
 SpeedArray = readtable(file);
 figure(1)
 PlotValues(GetValues(SpeedArray), SpeedArray,'Average Speed');
+XD = GetValues(SpeedArray);
 
 
 file = [a, "Stats\HearingDoc.csv"];
@@ -28,39 +29,60 @@ function [TempArray] = GetValues(Array)
    col1 = Array(:,1);
    col2 = Array(:,2);
    col3 = Array(:,3);
+   l1 = length(table2array(col1));
+   l2 = length(table2array(col2));
    C = length(unique(table2array(col1)));
    B = length(unique(table2array(col2)));
    pos = 1;
-   TempArray = zeros(C, B+1);
+   TempArray = zeros(C-1, B+1);
 
-    for i = 1:1:length(table2array(col1))
+    for i = 1:1:l1
        if mod(i,B) == 0
            TempArray(pos,B+1) = table2array(col3(i,1));
        end
-       if mod(i,B) ~= 0
+       if mod(i,B) ~= 0 && i ~= l1
        TempArray(pos,mod(i,B)+1) = table2array(col3(i,1));
        end
 
        if mod(i,B) == 0
             TempArray(pos,1) = table2array(col1(i,1));
             pos = pos + 1;
-        end     
+            RestOfList = Array(i:l2,2);
+            B = length(unique(table2array(RestOfList)));
+       end     
     end
 end
 
 function [] = PlotValues(TempArray, Array, Ylabel)
     [~,columns]= size(TempArray);
     NameCol = Array(:,2);
-    plot(TempArray(:,1),TempArray(:,2), 'DisplayName', join(char(table2cell(NameCol(1,1)))));
-    legend(join(char(table2cell(NameCol(1,1)))));
-    hold on
+    lx = find(TempArray(:,2)==0,1,'first');
+    NoZeroArray = TempArray(1:lx,2);
+    
+    if lx ~= 0  
+        plot(TempArray(1:lx,1),NoZeroArray, 'DisplayName', join(char(table2cell(NameCol(1,1)))));
+        legend(join(char(table2cell(NameCol(1,1)))));
+        hold on
+    else       
+        plot(TempArray(:,1),TempArray(:,2), 'DisplayName', join(char(table2cell(NameCol(1,1)))));
+        legend(join(char(table2cell(NameCol(1,1)))));
+        hold on
+    end
+
     
     for i = 2:1:columns
        
         if columns > i
-            plot(TempArray(:,1),TempArray(:,i+1), 'DisplayName', join(char(table2cell(NameCol(i,1)))));
-        end
+            
+            lx = find(TempArray(:,i+1)==0,1,'first');
+            NoZeroArray = TempArray(1:lx,i+1);
         
+            if lx ~= 0  
+                plot(TempArray(1:lx,1),NoZeroArray, 'DisplayName', join(char(table2cell(NameCol(i,1)))));
+            else       
+                plot(TempArray(:,1),TempArray(:,i+1), 'DisplayName', join(char(table2cell(NameCol(i,1)))));
+            end
+        end  
     end
     ylabel(Ylabel)
     xlabel('Time')      
