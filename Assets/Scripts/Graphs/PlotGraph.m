a1a51a5fd1da353303466a4cb8213e65bad9535b
emmanir <44803542+emmanir@users.noreply.github.com>
2020-05-11 13:12:21 +0200

    current values

--- Assets/Scripts/Graphs/PlotGraph.m
+++ Assets/Scripts/Graphs/PlotGraph.m
@@ -1,34 +1,34 @@
 clc;
 a = erase(pwd, "Ecosystem/Assets/Scripts/Graphs");
-file =[a, "Ecosystem/Stats/SpeedDoc.csv"];
-file = join(file, "/");
-SpeedArray = readtable(file);
-figure(1)
-PlotValues(GetValues(SpeedArray), SpeedArray,'Average Speed');
+%file =[a, "Ecosystem/Stats/SpeedDoc.csv"];
+%file = join(file, "/");
+%SpeedArray = readtable(file);
+%figure(1)
+%PlotValues(GetValues(SpeedArray), SpeedArray,'Average Speed');
 
-file = [a, "Ecosystem/Stats/HearingDoc.csv"];
-file = join(file, "/");
-HearingArray = readtable(file);
-figure(2)
-PlotValues(GetValues(HearingArray), HearingArray, 'Average Hearing Range');
+%file = [a, "Ecosystem/Stats/HearingDoc.csv"];
+%file = join(file, "/");
+%HearingArray = readtable(file);
+%figure(2)
+%PlotValues(GetValues(HearingArray), HearingArray, 'Average Hearing Range');
 
-file = [a, "Ecosystem/Stats/VisionDoc.csv"];
-file = join(file, "/");
-VisionArray = readtable(file);
-figure(3)
-PlotValues(GetValues(VisionArray), VisionArray, 'Average Vision Range');
+%file = [a, "Ecosystem/Stats/VisionDoc.csv"];
+%file = join(file, "/");
+%VisionArray = readtable(file);
+%figure(3)
+%PlotValues(GetValues(VisionArray), VisionArray, 'Average Vision Range');
 
-file = [a, "Ecosystem/Stats/AnimalCountDoc.csv"];
-file = join(file, "/");
-AnimalCountArray = readtable(file);
-figure(4)
-PlotValues(GetValues(AnimalCountArray), AnimalCountArray, 'Animal Count');
+%file = [a, "Ecosystem/Stats/AnimalCountDoc.csv"];
+%file = join(file, "/");
+%AnimalCountArray = readtable(file);
+%figure(4)
+%PlotValues(GetValues(AnimalCountArray), AnimalCountArray, 'Animal Count');
 
-file = [a, "Ecosystem/Stats/HungerLimitDoc.csv"];
-file = join(file, "/");
-AgeArray = readtable(file);
-figure(6)
-PlotValues(GetValues(AgeArray), AgeArray, 'Hunger Limit');
+%file = [a, "Ecosystem/Stats/HungerLimitDoc.csv"];
+%file = join(file, "/");
+%AgeArray = readtable(file);
+%figure(6)
+%PlotValues(GetValues(AgeArray), AgeArray, 'Hunger Limit');
 
 file = [a, "Ecosystem/Stats/ThirstLimitDoc.csv"];
 file = join(file, "/");
@@ -36,11 +36,11 @@ AgeArray = readtable(file);
 figure(7)
 PlotValues(GetValues(AgeArray), AgeArray, 'Thirst Limit');
 
-file = [a, "Ecosystem/Stats/MatingLimitDoc.csv"];
-file = join(file, "/");
-AgeArray = readtable(file);
-figure(8)
-PlotValues(GetValues(AgeArray), AgeArray, 'Mating Limit');
+%file = [a, "Ecosystem/Stats/MatingLimitDoc.csv"];
+%file = join(file, "/");
+%AgeArray = readtable(file);
+%figure(8)
+%PlotValues(GetValues(AgeArray), AgeArray, 'Mating Limit');
 
 function [TempArray] = GetValues(Array)
    col1 = Array(:,1);
@@ -90,20 +90,22 @@ function [] = PlotValues(TempArray, Array, Ylabel)
         hold on
     end
 
-    
     for i = 2:1:columns
-       
-        if columns > i
-            
-            lx = find(TempArray(:,i+1)==0,1,'first');
-            NoZeroArray = TempArray(1:lx-1,i+1);
-        
-            if lx ~= 0
-                plot(TempArray(1:lx-1,1)/3600,NoZeroArray, 'DisplayName', join(char(table2cell(NameCol(i,1)))),'LineWidth',2);
-            else       
-                plot(TempArray(:,1)/3600,TempArray(:,i+1), 'DisplayName', join(char(table2cell(NameCol(i,1)))),'LineWidth',2);
+        if strcmp(join(char(table2cell(NameCol(i,1)))),'Sardine')
+            plot([0],[0],'HandleVisibility','Off')
+        else
+            if columns > i
+
+                lx = find(TempArray(:,i+1)==0,1,'first');
+                NoZeroArray = TempArray(1:lx-1,i+1);
+
+                if lx ~= 0
+                    plot(TempArray(1:lx-1,1)/3600,NoZeroArray, 'DisplayName', join(char(table2cell(NameCol(i,1)))),'LineWidth',2);
+                else       
+                    plot(TempArray(:,1)/3600,TempArray(:,i+1), 'DisplayName', join(char(table2cell(NameCol(i,1)))),'LineWidth',2);
+                end
             end
-        end  
+        end
     end
     ylabel(Ylabel,'FontSize',20)
     xlabel('Time (in hours)','FontSize',20) 
